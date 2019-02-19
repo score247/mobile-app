dc6d30e7cbad78729a55e65184875ffd26d2b8f3
alice.le <alice.le@nexcel.vn>
2019-02-19 09:49:04 +0700

    108110- Build UI for MatchInfoPage

--- LiveScoreApp/Modules/Score/ViewModels/ScorePageViewModel.cs
+++ LiveScoreApp/Modules/Score/ViewModels/ScorePageViewModel.cs
@@ -1,10 +1,12 @@
 ﻿namespace Score.ViewModels
 {
+    using Prism.Commands;
     using Prism.Navigation;
     using Score.Models;
     using Score.Services;
     using System.Collections.ObjectModel;
     using System.Linq;
+    using Xamarin.Forms;
 
     public class ScorePageViewModel : ViewModelBase
     {
@@ -17,10 +19,13 @@
             set { SetProperty(ref groupMatches, value); }
         }
 
+        public DelegateCommand<string> NavigateCommand { get; set; }
+
         public ScorePageViewModel(INavigationService navigationService, IScoreService scoreService)
             : base(navigationService)
         {
             this.scoreService = scoreService;
+            NavigateCommand = new DelegateCommand<string>(Navigate);
         }
 
         public override void OnNavigatingTo(INavigationParameters parameters)
@@ -28,6 +33,11 @@
             var matches = scoreService.GetAll();
 
             GroupMatches = new ObservableCollection<IGrouping<string, Match>>(matches.GroupBy(x => x.GroupName));
+        }
+
+        private async void Navigate(string page)
+        {
+            await NavigationService.NavigateAsync(nameof(NavigationPage) + "/" + page, useModalNavigation: true);
         }
     }
 }
---
+++ LiveScoreApp/Modules/Score/Views/MatchInfoPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace Score.Views
+{
+    public partial class MatchInfoPage : TabbedPage
+    {
+        public MatchInfoPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
