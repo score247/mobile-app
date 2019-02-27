ef2db1142b75521b94ceade11ca14e36ba76108e
Harrison Nguyen <harrison.nguyen@starixsoft.com>
2019-02-27 16:37:39 +0700

    Separate out Resources from View

---
+++ LiveScoreApp/Modules/Score/ViewResources/ScorePageResource.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace Score.ViewResources
+{
+    using Xamarin.Forms;
+    using Xamarin.Forms.Xaml;
+
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ScorePageResource : ResourceDictionary
+    {
+        public ScorePageResource()
+        {
+            InitializeComponent();
+        }
+    }
+}
