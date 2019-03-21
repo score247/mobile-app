2fbd43b4165daab522e264573507606ddd5855fb
Harrison Nguyen <harrison.nguyen@starixsoft.com>
2019-03-21 15:17:37 +0700

    Create TVSchedule Module

---
+++ src/mobile/Modules/TVSchedule/TVScheduleModule.cs
@@ -0,0 +1,17 @@
+﻿namespace TVSchedule
+{
+    using Prism.Ioc;
+    using Prism.Modularity;
+
+    public class TVScheduleModule : IModule
+    {
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+        }
+    }
+}
