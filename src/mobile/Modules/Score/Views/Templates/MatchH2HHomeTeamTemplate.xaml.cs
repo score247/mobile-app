35eb302a122a634495dae39d175eb6ebe7fe9f9f
alice.le <alice.le@nexcel.vn>
2019-03-26 16:14:59 +0700

    110923-Create UI for MatchH2H

---
+++ src/mobile/Modules/Score/Views/Templates/MatchH2HHomeTeamTemplate.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Score.Views.Templates
+{
+    using Xamarin.Forms;
+
+    public partial class MatchH2HHomeTeamTemplate : ContentView
+    {
+        public MatchH2HHomeTeamTemplate()
+        {
+            InitializeComponent();
+        }
+    }
+}
