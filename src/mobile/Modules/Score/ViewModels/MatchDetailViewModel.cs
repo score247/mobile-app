aa7839343e1691a99949b35595b713ce1ff8a786
Vivian <nhavy,nguyen213@gmail.com>
2019-04-03 09:03:18 +0700

    Update match detail items

--- src/mobile/Modules/Score/ViewModels/MatchDetailViewModel.cs
+++ src/mobile/Modules/Score/ViewModels/MatchDetailViewModel.cs
@@ -11,37 +11,46 @@
     {
         private List<TabModel> matchDetailItems;
 
-        public MatchDetailViewModel(INavigationService navigationService)
-            : base(navigationService)
-        {
-            InitMatchDetailItems();
-        }
-
         public List<TabModel> MatchDetailItems
         {
             get { return matchDetailItems; }
             set { SetProperty(ref matchDetailItems, value); }
         }
 
-        private void InitMatchDetailItems()
+        public MatchDetailViewModel(INavigationService navigationService)
+            : base(navigationService)
         {
-            MatchDetailItems = new List<TabModel>
-            {
-                new TabModel {
-                    Id = 0,
-                    Name = "Trackers",
-                    Template = new MatchTrackerTemplate { BindingContext = new MatchViewModelBase(NavigationService) { MatchId = "1" } }
-                },
-            };
+            MatchDetailItems = new List<TabModel>();
         }
 
-        public override void OnNavigatedTo(INavigationParameters parameters)
+        public override void OnNavigatingTo(INavigationParameters parameters)
         {
             if (parameters != null)
             {
                 var match = parameters[nameof(Match)] as Match;
                 MatchId = match.Event.Id;
             }
+
+            if(MatchDetailItems.Count == 0)
+            {
+                InitMatchDetailItems(MatchId);
+            }
+        }
+
+        private void InitMatchDetailItems(string matchId)
+        {
+            MatchDetailItems = new List<TabModel>
+            {
+                new TabModel { Id = 0, Name = "Odds", Template = new MatchOdds1x2Template() },
+                new TabModel { Id = 1, Name = "Info", Template = new MatchInfoTemplate() },
+                new TabModel { Id = 2, Name = "Trackers", Template = new MatchTrackerTemplate { BindingContext = new MatchViewModelBase(NavigationService) { MatchId = matchId } } },
+                new TabModel { Id = 3, Name = "Stats", Template = new MatchStatsTemplate() },
+                new TabModel { Id = 4, Name = "LineUps", Template = new MatchLineUpsTemplate() },
+                new TabModel { Id = 5, Name = "H2H", Template = new MatchH2HAwayTeamTemplate() },
+                new TabModel { Id = 6, Name = "Table", Template = new MatchTableTotalFullListTemplate() },
+                new TabModel { Id = 7, Name = "Social", Template = new MatchInfoTemplate() },
+                new TabModel { Id = 8, Name = "TV Schedule", Template = new MatchInfoTemplate() }
+            };
         }
     }
 }
