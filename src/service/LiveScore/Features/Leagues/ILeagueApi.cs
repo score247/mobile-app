fffcf13565309e2a5457e0e43deac9701c52a392
ricky.nguyen <ricky.nguyen@starixsoft.com>
2019-04-08 16:33:56 +0700

    111138 - update sonarqube rules

--- src/service/LiveScore/Features/Leagues/ILeagueApi.cs
+++ src/service/LiveScore/Features/Leagues/ILeagueApi.cs
@@ -6,7 +6,7 @@
 
     public interface ILeagueApi
     {
-        [Get("/{sportId}-xt3/{group}/{lang}/schedules/{date}/results.json?api_key={key}")]
+        //[Get("/{sportId}-xt3/{group}/{lang}/schedules/{date}/results.json?api_key={key}")]
         Task<DailyScheduleDto> GetDailySchedules(string sportName, string group, string lang, string date, string key);
     }
 }
