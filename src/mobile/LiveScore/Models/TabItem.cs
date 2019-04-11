03a981ef0fa8f15628d8db90e78332f317c25f3a
Vivian <vivian.nguyen@starixsoft.com>
2019-04-11 09:28:53 +0700

    111718 - add custom More page

---
+++ src/mobile/LiveScore/Models/TabItem.cs
@@ -0,0 +1,12 @@
+﻿namespace LiveScore.Models
+{
+    public class TabItem
+    {
+        public TabItem(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; set; }
+    }
+}
--- src/mobile/LiveScore/ViewModels/TabMoreViewModel.cs
+++ src/mobile/LiveScore/ViewModels/TabMoreViewModel.cs
@@ -1,15 +1,24 @@
 ﻿namespace LiveScore.ViewModels
 {
+    using System.Collections.ObjectModel;
     using LiveScore.Core.Factories;
     using LiveScore.Core.Services;
     using LiveScore.Core.ViewModels;
+    using LiveScore.Models;
     using Prism.Navigation;
 
     public class TabMoreViewModel : ViewModelBase
     {
-        public TabMoreViewModel(INavigationService navigationService, IGlobalFactoryProvider globalFactory, ISettingsService settingsService) 
+        public ObservableCollection<TabItem> TabItems { get; set; }
+
+        public TabMoreViewModel(INavigationService navigationService, IGlobalFactoryProvider globalFactory, ISettingsService settingsService)
             : base(navigationService, globalFactory, settingsService)
         {
+            TabItems = new ObservableCollection<TabItem> 
+            { 
+                new TabItem("News"), 
+                new TabItem("TV") 
+            };
         }
     }
 }
