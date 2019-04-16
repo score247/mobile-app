bf3c48c7df40982e7043189df4696124400ee3e4
Vivian <vivian.nguyen@starixsoft.com>
2019-04-16 16:29:25 +0700

    111751 - update format datetime

--- src/Shared/Common/Extensions/DateTimeExtension.cs
+++ src/Shared/Common/Extensions/DateTimeExtension.cs
@@ -5,7 +5,7 @@
     public static class DateTimeExtension
     {
         public static string ToApiFormat(this DateTime value)
-            => value.ToString("yyyy-MM-dd");
+            => value.ToString("yyyy-MM-ddTHH:mm:sszzz");
 
         public static string ToShortDayMonth(this DateTime value)
             => value.ToString("dd MMM");
