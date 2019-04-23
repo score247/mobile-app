bc99bae0bf39732666221149acd2811c8a4e8da9
Vivian.Nguyen <vivian.nguyen@nexcel.vn>
2019-04-23 18:06:08 +0700

    112260 - update unit tests

---
+++ LiveScore.Tests/ViewModels/MainViewModelTests.cs
@@ -0,0 +1,37 @@
+﻿using LiveScore.Core;
+using LiveScore.ViewModels;
+using NSubstitute;
+using Prism.Navigation;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace LiveScore.Tests.ViewModels
+{
+    public class MainViewModelTests
+    {
+        private readonly INavigationService mockNavigation;
+        private readonly IDepdendencyResolver mockResolver;
+
+        private readonly MainViewModel viewModel;
+
+        public MainViewModelTests()
+        {
+            mockNavigation = Substitute.For<INavigationService>();
+            mockResolver = Substitute.For<IDepdendencyResolver>();
+
+            viewModel = new MainViewModel(mockNavigation, mockResolver);
+        }
+
+        [Fact]
+        public async Task NavigateCommand_Execute_InjectNavigationService()
+        {
+            // Arrange
+
+            // Act
+            //await viewModel.NavigateCommand.ExecuteAsync("page");
+
+            // Assert
+            //await mockNavigation.Received(1).NavigateAsync(Arg.Any<string>(), Arg.Any<INavigationParameters>(), Arg.Any<bool>(), Arg.Any<bool>());
+        }
+    }
+}
