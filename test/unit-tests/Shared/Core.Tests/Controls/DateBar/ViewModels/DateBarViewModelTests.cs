4869967bba6a6a3270dd3c7431ac9683456cec39
Vivian.Nguyen <vivian.nguyen@nexcel.vn>
2019-04-24 09:35:51 +0700

    112260 - add DateBarViewModelTests

---
+++ test/unit-tests/Shared/Core.Tests/Controls/DateBar/ViewModels/DateBarViewModelTests.cs
@@ -0,0 +1,34 @@
+﻿using LiveScore.Core.Controls.DateBar.ViewModels;
+using LiveScore.Core.Tests.Fixtures;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace LiveScore.Core.Tests.Controls.DateBar.ViewModels
+{
+    public class DateBarViewModelTests
+    {
+        private readonly DateBarViewModel viewModel;
+
+        public DateBarViewModelTests()
+        {
+            viewModel = new DateBarViewModel();
+        }
+
+        [Fact]
+        public void RenderCalendarItems_ShouldReturnListBaseOnNumberDisplayDays()
+        {
+            // Arrange
+            viewModel.NumberOfDisplayDays = 3;
+            var expectedCount = viewModel.NumberOfDisplayDays * 2 + 1;
+
+            // Act
+            viewModel.RenderCalendarItems();
+            var actualCount = viewModel.CalendarItems.Count;
+
+            // Assert
+            Assert.Equal(expectedCount, actualCount);
+        }
+    }
+}
