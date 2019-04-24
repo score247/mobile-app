abf980d0b11e817ac1b1c911f16df84163566178
Harrison Nguyen <harrison.nguyen@starixsoft.com>
2019-04-24 17:40:23 +0700

    112091 - Remove unused LiveViewModel

--- src/Features/Score/ViewModels/LiveViewModel.cs
+++ src/Features/Score/ViewModels/LiveViewModel.cs
@@ -1,12 +1,7 @@
 ﻿namespace LiveScore.Score.ViewModels
 {
-    using System.Collections.Generic;
-    using System.Collections.ObjectModel;
-    using System.Linq;
     using Core.ViewModels;
     using LiveScore.Core;
-    using LiveScore.Core.Models.Matches;
-    using Prism.Commands;
     using Prism.Navigation;
 
     public class LiveViewModel : ViewModelBase
@@ -14,29 +9,6 @@
         public LiveViewModel(INavigationService navigationService, IDepdendencyResolver serviceLocator)
             : base(navigationService, serviceLocator)
         {
-        }
-
-        public ObservableCollection<IGrouping<string, IMatch>> GroupMatches { get; set; }
-
-        public bool IsRefreshingMatchList { get; set; }
-
-        public DelegateCommand RefreshCommand
-            => new DelegateCommand(() =>
-            {
-                IsRefreshingMatchList = true;
-                GetMatches();
-                IsRefreshingMatchList = false;
-            });
-
-        public override void OnAppearing()
-        {
-            GetMatches();
-        }
-
-        private void GetMatches()
-        {
-            var matches = new List<IMatch>();
-            GroupMatches = new ObservableCollection<IGrouping<string, IMatch>>(matches.GroupBy(x => x.League.Id));
         }
     }
 }
