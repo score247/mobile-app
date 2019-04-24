367b1db88c271c3fa7ad6f6bcc40b2fd89872d14
Vivian.Nguyen <vivian.nguyen@nexcel.vn>
2019-04-24 10:03:13 +0700

    112260 - update DateBarViewModelTests

--- test/unit-tests/Shared/Core.Tests/Controls/DateBar/ViewModels/DateBarViewModelTests.cs
+++ test/unit-tests/Shared/Core.Tests/Controls/DateBar/ViewModels/DateBarViewModelTests.cs
@@ -1,19 +1,26 @@
-﻿using LiveScore.Core.Controls.DateBar.ViewModels;
+﻿using LiveScore.Core.Controls.DateBar.Events;
+using LiveScore.Core.Controls.DateBar.Models;
+using LiveScore.Core.Controls.DateBar.ViewModels;
 using LiveScore.Core.Tests.Fixtures;
+using NSubstitute;
+using Prism.Events;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
 using Xunit;
 
 namespace LiveScore.Core.Tests.Controls.DateBar.ViewModels
 {
-    public class DateBarViewModelTests
-    {
+    public class DateBarViewModelTests : IClassFixture<ViewModelBaseFixture>
+    {        
         private readonly DateBarViewModel viewModel;
 
-        public DateBarViewModelTests()
+        public DateBarViewModelTests(ViewModelBaseFixture viewModelBaseFixture)
         {
             viewModel = new DateBarViewModel();
+            viewModel.EventAggregator = viewModelBaseFixture.EventAggregator;
+            viewModel.SettingsService = viewModelBaseFixture.AppSettingsFixture.SettingsService;
         }
 
         [Fact]
@@ -29,6 +36,35 @@ namespace LiveScore.Core.Tests.Controls.DateBar.ViewModels
 
             // Assert
             Assert.Equal(expectedCount, actualCount);
+        }
+
+        [Fact]
+        public void OnSelectHome_SelectingHome_NotGetEvent()
+        {
+            // Arrange
+            viewModel.HomeIsSelected = true;
+
+            // Act
+            viewModel.SelectHomeCommand.Execute();
+
+            // Assert
+            viewModel.EventAggregator.DidNotReceive().GetEvent<DateBarItemSelectedEvent>();
+        }
+
+        [Fact]
+        public void OnSelectHome_NotSelectingHome_InjectEventAggregator()
+        {
+            // Arrange
+            viewModel.HomeIsSelected = false;
+            viewModel.NumberOfDisplayDays = 2;
+            viewModel.RenderCalendarItems();
+            viewModel.EventAggregator.GetEvent<DateBarItemSelectedEvent>().Returns(new DateBarItemSelectedEvent());
+
+            // Act
+            viewModel.SelectHomeCommand.Execute();
+
+            // Assert
+            viewModel.EventAggregator.Received(1).GetEvent<DateBarItemSelectedEvent>();
         }
     }
 }
