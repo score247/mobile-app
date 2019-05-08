2970b596a7a1d3c6140f4360674f0ce3cd36ee1d
Harrison Nguyen <harrison.nguyen@starixsoft.com>
2019-05-08 13:24:33 +0700

    112954 - Add env for api

--- src/Shared/Common/Configuration/Configuration.cs
+++ src/Shared/Common/Configuration/Configuration.cs
@@ -2,8 +2,13 @@
 {
     public static class Configuration
     {
+#if DEV
         public static string LocalEndPoint => "https://testing2.nexdev.net/V1/api/";
-
+#elif TEST
+        public static string LocalEndPoint => "https://testing2.nexdev.net/Test/api/";
+#else
+        public static string LocalEndPoint => "https://testing2.nexdev.net/Main/api/";
+#endif
         public static string SentryDsn => "https://a75e3e7b51ea4de8baa2c27b67bbede3@sentry.nexdev.net/34";
     }
 }
