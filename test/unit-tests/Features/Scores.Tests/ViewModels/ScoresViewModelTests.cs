9b1bbdea96d42c7bb4767e5cd0e7e83e71966d38
Harrison Nguyen <harrison.nguyen@starixsoft.com>
2019-05-28 13:40:15 +0700

    Update test

--- test/unit-tests/Features/Scores.Tests/ViewModels/ScoresViewModelTests.cs
+++ test/unit-tests/Features/Scores.Tests/ViewModels/ScoresViewModelTests.cs
@@ -173,7 +173,7 @@ namespace Scores.Tests.ViewModels
             viewModel.OnAppearing();
 
             // Assert
-            await viewModel.LoggingService.Received(1).LogErrorAsync(Arg.Any<Exception>());
+            await viewModel.LoggingService.ReceivedWithAnyArgs(1).LogErrorAsync(Arg.Any<Exception>());
         }
 
         [Fact]
