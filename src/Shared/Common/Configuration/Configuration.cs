fd868ef8f2f4bdf9d0e208755b70913067572463
Vivian <vivian.nguyen@starixsoft.com>
2019-06-03 13:06:45 +0700

    000 - switch to testing2 domain due to invalid cert

--- src/Shared/Common/Configuration/Configuration.cs
+++ src/Shared/Common/Configuration/Configuration.cs
@@ -3,17 +3,17 @@
     public static class Configuration
     {
 #if DEBUG
-        public static string LocalEndPoint => "https://api.nexdev.net/V1/api/";
+        public static string LocalEndPoint => "https://testing2.nexdev.net/V1/api/";
 
-        public static string LocalHubEndPoint => "https://api.nexdev.net/V1/hubs/";
+        public static string LocalHubEndPoint => "https://testing2.nexdev.net/V1/hubs/";
 #elif TEST
-        public static string LocalEndPoint => "https://api.nexdev.net/V2/api/";
+        public static string LocalEndPoint => "https://testing2.nexdev.net/V2/api/";
 
-         public static string LocalHubEndPoint => "https://api.nexdev.net/V2/hubs/";
+         public static string LocalHubEndPoint => "https://testing2.nexdev.net/V2/hubs/";
 #else
-        public static string LocalEndPoint => "https://api.nexdev.net/Main/api/";
+        public static string LocalEndPoint => "https://testing2.nexdev.net/Main/api/";
 
-         public static string LocalHubEndPoint => "https://api.nexdev.net/Main/hubs/";
+         public static string LocalHubEndPoint => "https://testing2.nexdev.net/Main/hubs/";
 #endif
         public static string SentryDsn => "https://a75e3e7b51ea4de8baa2c27b67bbede3@sentry.nexdev.net/34";
     }
