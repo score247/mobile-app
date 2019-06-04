a2a9b9b001b72787467207ef2354c82c1f2d482c
Vivian <vivian.nguyen@starixsoft.com>
2019-06-04 10:15:46 +0700

    000 - switch api domain

--- test/unit-tests/Shared/Common.Tests/Configuration/ConfigurationTests.cs
+++ test/unit-tests/Shared/Common.Tests/Configuration/ConfigurationTests.cs
@@ -9,7 +9,7 @@ namespace LiveScore.Common.Tests.Configuration
         public void LocalEndPoint_Always_GetExpectedValue()
         {
             // Assert
-            Assert.Equal("https://testing2.nexdev.net/V1/api/", Configuration.LocalEndPoint);
+            Assert.Equal("https://api.nexdev.net/V1/api/", Configuration.LocalEndPoint);
         }
 
         [Fact]
