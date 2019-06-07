2d8b36b1a24ee5df3d18d7f5ccbfb693f7251514
Harrison Nguyen <harrison.nguyen@starixsoft.com>
2019-06-07 11:50:46 +0700

    Remove select sport view

--- src/Shared/Core/ViewModels/NavigationTitleViewModel.cs
+++ src/Shared/Core/ViewModels/NavigationTitleViewModel.cs
@@ -1,9 +1,6 @@
 ﻿namespace LiveScore.Core.ViewModels
 {
-    using System.Threading.Tasks;
-    using Common.Extensions;
     using LiveScore.Core;
-    using LiveScore.Core.Views;
     using Prism.Navigation;
 
     public class NavigationTitleViewModel : ViewModelBase
@@ -11,15 +8,9 @@
         public NavigationTitleViewModel(INavigationService navigationService, IDependencyResolver serviceLocator)
             : base(navigationService, serviceLocator)
         {
-            SelectSportCommand = new DelegateAsyncCommand(NavigateSelectSportPage);
             CurrentSportName = SettingsService.CurrentSportType.DisplayName;
         }
 
         public string CurrentSportName { get; }
-
-        public DelegateAsyncCommand SelectSportCommand { get; }
-
-        private async Task NavigateSelectSportPage()
-            => await NavigationService.NavigateAsync("NavigationPage/" + nameof(SelectSportView), useModalNavigation: true);
     }
 }
