9c503f8e4be1a376c7798f46ea6381eb36d163e7
Harrison Nguyen <harrison.nguyen@starixsoft.com>
2019-06-26 20:07:44 +0700

    115303 - Fix sonarqube

--- src/Shared/Core/Controls/TabStrip/TabStrip.xaml.cs
+++ src/Shared/Core/Controls/TabStrip/TabStrip.xaml.cs
@@ -1,111 +1,112 @@
 ﻿namespace LiveScore.Core.Controls.TabStrip
 {
-	using System.Collections.Generic;
-	using System.Linq;
-	using LiveScore.Core.ViewModels;
-	using Xamarin.Forms;
-	using Xamarin.Forms.Xaml;
+    using System.Collections.Generic;
+    using System.Linq;
+    using LiveScore.Core.ViewModels;
+    using Xamarin.Forms;
+    using Xamarin.Forms.Xaml;
 
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class TabStrip : ContentView
-	{
-		private static int currentTabIndex;
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TabStrip : ContentView
+    {
+        private const string TabChangeEvent = "TabChange";
+        private static int currentTabIndex;
 
-		public TabStrip()
-		{
-			var currentInstance = this;
+        public TabStrip()
+        {
+            var currentInstance = this;
 
-			InitializeComponent();
-			TabHeader.BindingContext = currentInstance;
-		}
+            InitializeComponent();
+            TabHeader.BindingContext = currentInstance;
+        }
 
-		public static readonly BindableProperty ItemsSourceProperty = BindableProperty.Create(
-			"ItemsSource",
-			typeof(IEnumerable<TabModel>),
-			typeof(TabStrip),
-			propertyChanged: OnItemsSourceChanged);
+        public static readonly BindableProperty ItemsSourceProperty = BindableProperty.Create(
+            nameof(ItemsSource),
+            typeof(IEnumerable<TabModel>),
+            typeof(TabStrip),
+            propertyChanged: OnItemsSourceChanged);
 
-		public IEnumerable<TabModel> ItemsSource
-		{
-			get { return (IEnumerable<TabModel>)GetValue(ItemsSourceProperty); }
-			set { SetValue(ItemsSourceProperty, value); }
-		}
+        public IEnumerable<TabModel> ItemsSource
+        {
+            get { return (IEnumerable<TabModel>)GetValue(ItemsSourceProperty); }
+            set { SetValue(ItemsSourceProperty, value); }
+        }
 
-		private static void OnItemsSourceChanged(BindableObject bindable, object oldValue, object newValue)
-		{
-			var control = (TabStrip)bindable;
+        private static void OnItemsSourceChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            var control = (TabStrip)bindable;
 
-			if (control == null || newValue == null)
-			{
-				MessagingCenter.Unsubscribe<string, int>(nameof(TabStrip), "TabChange");
-				currentTabIndex = 0;
-				return;
-			}
+            if (control == null || newValue == null)
+            {
+                MessagingCenter.Unsubscribe<string, int>(nameof(TabStrip), TabChangeEvent);
+                currentTabIndex = 0;
+                return;
+            }
 
-			var tabs = (IEnumerable<TabModel>)newValue;
+            var tabs = (IEnumerable<TabModel>)newValue;
 
-			if (oldValue == null)
-			{
-				InitDefaultTab(control, tabs);
-			}
+            if (oldValue == null)
+            {
+                InitDefaultTab(control, tabs);
+            }
 
-			SubscribeTabChange(control, tabs);
-		}
+            SubscribeTabChange(control, tabs);
+        }
 
-		private static void InitDefaultTab(TabStrip control, IEnumerable<TabModel> tabs)
-		{
-			control.TabContent.Children.Clear();
-			control.TabContent.Children.Add(new ContentView
-			{
-				Content = tabs.First().Template,
-				BindingContext = tabs.First().ViewModel
-			});
-			tabs.First().ViewModel.OnAppearing();
-		}
+        private static void InitDefaultTab(TabStrip control, IEnumerable<TabModel> tabs)
+        {
+            control.TabContent.Children.Clear();
+            control.TabContent.Children.Add(new ContentView
+            {
+                Content = tabs.First().Template,
+                BindingContext = tabs.First().ViewModel
+            });
+            tabs.First().ViewModel.OnAppearing();
+        }
 
-		private static void SubscribeTabChange(TabStrip control, IEnumerable<TabModel> tabs)
-		{
-			MessagingCenter.Subscribe<string, int>(nameof(TabStrip), "TabChange", (_, index) =>
-			{
-				currentTabIndex = index;
-				var tab = tabs.ToArray()[index];
+        private static void SubscribeTabChange(TabStrip control, IEnumerable<TabModel> tabs)
+        {
+            MessagingCenter.Subscribe<string, int>(nameof(TabStrip), TabChangeEvent, (_, index) =>
+            {
+                currentTabIndex = index;
+                var tab = tabs.ToArray()[index];
 
-				control.TabContent.Children.ToList()
-					.ForEach(c => (c.BindingContext as ViewModelBase)?.OnDisappearing());
-				control.TabContent.Children.Clear();
-				control.TabContent.Children.Add(new ContentView
-				{
-					Content = tab.Template,
-					BindingContext = tab.ViewModel
-				});
+                control.TabContent.Children.ToList()
+                    .ForEach(c => (c.BindingContext as ViewModelBase)?.OnDisappearing());
+                control.TabContent.Children.Clear();
+                control.TabContent.Children.Add(new ContentView
+                {
+                    Content = tab.Template,
+                    BindingContext = tab.ViewModel
+                });
 
-				tab.ViewModel.OnAppearing();
-			});
-		}
+                tab.ViewModel.OnAppearing();
+            });
+        }
 
-		private void OnSwiped(object sender, SwipedEventArgs e)
-		{
-			if (e.Direction == SwipeDirection.Left)
-			{
-				var newIndex = currentTabIndex + 1;
+        private void OnSwiped(object sender, SwipedEventArgs e)
+        {
+            if (e.Direction == SwipeDirection.Left)
+            {
+                var newIndex = currentTabIndex + 1;
 
-				if (newIndex < ItemsSource.Count())
-				{
-					MessagingCenter.Send("Tab", "TabChange", newIndex);
-				}
-			}
-			else
-			{
-				if (e.Direction == SwipeDirection.Right)
-				{
-					var newIndex = currentTabIndex - 1;
+                if (newIndex < ItemsSource.Count())
+                {
+                    MessagingCenter.Send(nameof(TabStrip), TabChangeEvent, newIndex);
+                }
+            }
+            else
+            {
+                if (e.Direction == SwipeDirection.Right)
+                {
+                    var newIndex = currentTabIndex - 1;
 
-					if (newIndex >= 0)
-					{
-						MessagingCenter.Send("Tab", "TabChange", newIndex);
-					}
-				}
-			}
-		}
-	}
+                    if (newIndex >= 0)
+                    {
+                        MessagingCenter.Send(nameof(TabStrip), TabChangeEvent, newIndex);
+                    }
+                }
+            }
+        }
+    }
 }
