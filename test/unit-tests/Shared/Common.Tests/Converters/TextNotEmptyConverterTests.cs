4be758a0143ab25437bdb54bc8b1b3325afd33d9
Harrison Nguyen <harrison.nguyen@starixsoft.com>
2019-07-08 13:22:44 +0700

    116328 - Add more tests

---
+++ test/unit-tests/Shared/Common.Tests/Converters/TextNotEmptyConverterTests.cs
@@ -0,0 +1,34 @@
+namespace LiveScore.Common.Tests.Converters
+{
+    using LiveScore.Common.Converters;
+    using Xunit;
+
+    public class TextNotEmptyConverterTests
+    {
+        [Fact]
+        public void Convert_ValueIsNotNullOrEmpty_ReturnTrue()
+        {
+            // Arrange
+            var textNotEmtpyConverter = new TextNotEmptyConverter();
+
+            // Act
+            var actual = (bool)textNotEmtpyConverter.Convert("test", null, null, null);
+
+            // Assert
+            Assert.True(actual);
+        }
+
+        [Fact]
+        public void Convert_ValueIsNotNull_ReturnFalse()
+        {
+            // Arrange
+            var textNotEmtpyConverter = new TextNotEmptyConverter();
+
+            // Act
+            var actual = (bool)textNotEmtpyConverter.Convert(null, null, null, null);
+
+            // Assert
+            Assert.False(actual);
+        }
+    }
+}
