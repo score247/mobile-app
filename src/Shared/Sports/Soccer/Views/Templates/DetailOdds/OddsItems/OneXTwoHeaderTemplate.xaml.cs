258369c9faaa07c3ed59b68114caa4fdb476b0ee
Vivian <vivian.nguyen@starixsoft.com>
2019-07-09 18:13:09 +0700

    115948 - update odds header template

---
+++ src/Shared/Sports/Soccer/Views/Templates/DetailOdds/OddsItems/OneXTwoHeaderTemplate.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace LiveScore.Soccer.Views.Templates.DetailOdds.OddsItems
+{
+    using Xamarin.Forms;
+
+    public partial class OneXTwoHeaderTemplate : DataTemplate
+    {
+        public OneXTwoHeaderTemplate()
+        {
+            InitializeComponent();
+        }
+    }
+}
