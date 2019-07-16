6a8251983d642875c8ff1ddde958b0619a303b75
Vivian <vivian.nguyen@starixsoft.com>
2019-07-16 17:12:55 +0700

    116918 - update unit tests

--- src/Shared/Sports/Soccer.Tests/ViewModels/DetailOdds/OddsItems/AsianHdpItemViewModeltests.cs
+++ src/Shared/Sports/Soccer.Tests/ViewModels/DetailOdds/OddsItems/AsianHdpItemViewModeltests.cs
@@ -1,29 +1,28 @@
 ﻿namespace Soccer.Tests.ViewModels.DetailOdds.OddsItems
 {
+    using System.Collections.Generic;
     using LiveScore.Core.Enumerations;
     using LiveScore.Core.Models.Odds;
     using LiveScore.Core.Tests.Fixtures;
     using LiveScore.Soccer.Enumerations;
     using LiveScore.Soccer.ViewModels.DetailOdds.OddItems;
     using NSubstitute;
-    using System.Collections.Generic;
     using Xunit;
 
-    public class OneXTwoViewModelTests : IClassFixture<ViewModelBaseFixture>, IClassFixture<ResourcesFixture>
+    public class AsianHdpItemViewModeltests : IClassFixture<ViewModelBaseFixture>, IClassFixture<ResourcesFixture>
     {
 
         private readonly ViewModelBaseFixture baseFixture;
         private readonly IBetTypeOdds betTypeOdds;
 
-        public OneXTwoViewModelTests(ViewModelBaseFixture baseFixture)
+        public AsianHdpItemViewModeltests(ViewModelBaseFixture baseFixture)
         {
             betTypeOdds = Substitute.For<IBetTypeOdds>();
             betTypeOdds.Bookmaker.Returns(new Bookmaker { Id = "book1", Name = "book1" });
             betTypeOdds.BetOptions.Returns(new List<BetOptionOdds>
             {
-                new BetOptionOdds{ Type = "home", LiveOdds = 5.000m, OpeningOdds = 4.900m, OddsTrend = OddsTrend.Up },
-                new BetOptionOdds{ Type = "draw", LiveOdds = 3.200m, OpeningOdds = 3.200m, OddsTrend = OddsTrend.Neutral },
-                new BetOptionOdds{ Type = "away", LiveOdds = 2.500m, OpeningOdds = 2.800m, OddsTrend = OddsTrend.Down }
+                new BetOptionOdds{ Type = "home", LiveOdds = 5.000m, OpeningOdds = 4.900m, OddsTrend = OddsTrend.Up, OpeningOptionValue= "1.25", OptionValue = "1.5" },               
+                new BetOptionOdds{ Type = "away", LiveOdds = 2.500m, OpeningOdds = 2.800m, OddsTrend = OddsTrend.Down, OpeningOptionValue= "2", OptionValue = "2.5" }
             });
 
             this.baseFixture = baseFixture;
@@ -33,7 +32,7 @@
         public void HomeLiveOdds_Always_GetExpectedFormat()
         {
             // Arrange               
-            var viewModel = new OneXTwoItemViewModel(BetType.OneXTwo, betTypeOdds, baseFixture.NavigationService, baseFixture.DependencyResolver);
+            var viewModel = new AsianHdpItemViewModel(BetType.AsianHDP, betTypeOdds, baseFixture.NavigationService, baseFixture.DependencyResolver);
 
             // Act
             viewModel.CreateInstance();
@@ -43,23 +42,24 @@
         }
 
         [Fact]
-        public void DrawLiveOdds_Always_GetExpectedFormat()
+        public void HdpValue_Always_GetExpectedFormat()
         {
             // Arrange               
-            var viewModel = new OneXTwoItemViewModel(BetType.OneXTwo, betTypeOdds, baseFixture.NavigationService, baseFixture.DependencyResolver);
+            var viewModel = new AsianHdpItemViewModel(BetType.AsianHDP, betTypeOdds, baseFixture.NavigationService, baseFixture.DependencyResolver);
 
             // Act
             viewModel.CreateInstance();
 
             // Assert
-            Assert.Equal("3.20", viewModel.DrawLiveOdds);
+            Assert.Equal("1.25", viewModel.OpeningHdp);
+            Assert.Equal("1.5", viewModel.LiveHdp);
         }
 
         [Fact]
         public void AwayLiveOdds_Always_GetExpectedFormat()
         {
             // Arrange               
-            var viewModel = new OneXTwoItemViewModel(BetType.OneXTwo, betTypeOdds, baseFixture.NavigationService, baseFixture.DependencyResolver);
+            var viewModel = new AsianHdpItemViewModel(BetType.AsianHDP, betTypeOdds, baseFixture.NavigationService, baseFixture.DependencyResolver);
 
             // Act
             viewModel.CreateInstance();
---
+++ src/Shared/Sports/Soccer.Tests/ViewModels/DetailOdds/OddsItems/OverUnderItemViewModelTests.cs
@@ -0,0 +1,71 @@
+﻿namespace Soccer.Tests.ViewModels.DetailOdds.OddsItems
+{
+    using System.Collections.Generic;
+    using LiveScore.Core.Enumerations;
+    using LiveScore.Core.Models.Odds;
+    using LiveScore.Core.Tests.Fixtures;
+    using LiveScore.Soccer.Enumerations;
+    using LiveScore.Soccer.ViewModels.DetailOdds.OddItems;
+    using NSubstitute;
+    using Xunit;
+
+    public class OverUnderItemViewModelTests : IClassFixture<ViewModelBaseFixture>, IClassFixture<ResourcesFixture>
+    {
+
+        private readonly ViewModelBaseFixture baseFixture;
+        private readonly IBetTypeOdds betTypeOdds;
+
+        public OverUnderItemViewModelTests(ViewModelBaseFixture baseFixture)
+        {
+            betTypeOdds = Substitute.For<IBetTypeOdds>();
+            betTypeOdds.Bookmaker.Returns(new Bookmaker { Id = "book1", Name = "book1" });
+            betTypeOdds.BetOptions.Returns(new List<BetOptionOdds>
+            {
+                new BetOptionOdds{ Type = "over", LiveOdds = 5.000m, OpeningOdds = 4.900m, OddsTrend = OddsTrend.Up, OpeningOptionValue= "1.25", OptionValue = "1.5" },
+                new BetOptionOdds{ Type = "under", LiveOdds = 2.500m, OpeningOdds = 2.800m, OddsTrend = OddsTrend.Down, OpeningOptionValue= "2", OptionValue = "2.5" }
+            });
+
+            this.baseFixture = baseFixture;
+        }
+
+        [Fact]
+        public void OverLiveOdds_Always_GetExpectedFormat()
+        {
+            // Arrange               
+            var viewModel = new OverUnderItemViewModel(BetType.OverUnder, betTypeOdds, baseFixture.NavigationService, baseFixture.DependencyResolver);
+
+            // Act
+            viewModel.CreateInstance();
+
+            // Assert
+            Assert.Equal("5.00", viewModel.OverLiveOdds);
+        }
+
+        [Fact]
+        public void OptionValue_Always_GetExpectedFormat()
+        {
+            // Arrange               
+            var viewModel = new OverUnderItemViewModel(BetType.OverUnder, betTypeOdds, baseFixture.NavigationService, baseFixture.DependencyResolver);
+
+            // Act
+            viewModel.CreateInstance();
+
+            // Assert
+            Assert.Equal("1.25", viewModel.OpeningOverOptionValue);
+            Assert.Equal("1.5", viewModel.LiveOverOptionValue);
+        }
+
+        [Fact]
+        public void UnderLiveOdds_Always_GetExpectedFormat()
+        {
+            // Arrange               
+            var viewModel = new OverUnderItemViewModel(BetType.OverUnder, betTypeOdds, baseFixture.NavigationService, baseFixture.DependencyResolver);
+
+            // Act
+            viewModel.CreateInstance();
+
+            // Assert
+            Assert.Equal("2.50", viewModel.UnderLiveOdds);
+        }
+    }
+}
