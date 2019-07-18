328dda876a9eeccdf7e50cb2411f6b32c22ea6c3
Vivian <vivian.nguyen@starixsoft.com>
2019-07-18 09:46:02 +0700

    116918 - move tab converters to Core

---
+++ src/Shared/Core/Converters/SelectedTabBackgroundColorConverter.cs
@@ -0,0 +1,28 @@
+﻿namespace LiveScore.Core.Converters
+{
+    using System;
+    using System.Globalization;
+    using Xamarin.Forms;
+
+    public class SelectedTabBackgroundColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var color = (Color)Application.Current.Resources["SubTabBgColor"];
+
+            if (value != null)
+            {
+                bool isSelected = (bool)value;
+
+                color = isSelected ? (Color)Application.Current.Resources["ActiveSubTabBgColor"] : (Color)Application.Current.Resources["SubTabBgColor"];
+            }
+
+            return color;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
---
+++ src/Shared/Core/Converters/SelectedTabColorConverter.cs
@@ -0,0 +1,28 @@
+﻿namespace LiveScore.Core.Converters
+{
+    using System;
+    using System.Globalization;
+    using Xamarin.Forms;
+
+    public class SelectedTabColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var color = (Color)Application.Current.Resources["SubTabColor"];
+
+            if (value != null)
+            {
+                bool isSelected = (bool)value;
+
+                color = isSelected ? (Color)Application.Current.Resources["ActiveSubTabColor"] : (Color)Application.Current.Resources["SubTabColor"];
+            }
+
+            return color;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
