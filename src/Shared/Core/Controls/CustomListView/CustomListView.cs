44ab860d3d50b199f92087dfbe59c3cd9d8cdcd7
Harrison Nguyen <harrison.nguyen@starixsoft.com>
2019-07-19 11:49:28 +0700

    Add automation id for auto test

---
+++ src/Shared/Core/Controls/CustomListView/CustomListView.cs
@@ -0,0 +1,21 @@
+﻿namespace LiveScore.Core.Controls.CustomListView
+{
+    using Xamarin.Forms;
+
+    public class CustomListView : ListView
+    {
+#if TEST || AUTOTEST
+
+        public CustomListView() : base(ListViewCachingStrategy.RetainElement)
+        {
+        }
+
+#else
+
+        public CustomListView() : base(ListViewCachingStrategy.RecycleElement)
+        {
+        }
+
+#endif
+    }
+}
