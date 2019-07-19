18e3a4bb2c61f27f1d982cdcc2f96b752a6992bc
Vivian <vivian.nguyen@starixsoft.com>
2019-07-19 16:41:19 +0700

    116920 - update template selector

--- src/Shared/Sports/Soccer/Views/Templates/DetailOdds/OddsMovementItemTemplateSelector.cs
+++ src/Shared/Sports/Soccer/Views/Templates/DetailOdds/OddsMovementItemTemplateSelector.cs
@@ -1,13 +1,15 @@
 ﻿namespace LiveScore.Soccer.Views.Templates.DetailOdds
 {
-    using LiveScore.Soccer.Views.Templates.DetailOdds.OddsItems;
+    using LiveScore.Soccer.ViewModels.DetailOdds.OddItems;
     using Xamarin.Forms;
 
     public class OddsMovementItemTemplateSelector : DataTemplateSelector
     {
         protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
         {
-            return new OneXTwoMovementItemTemplate();
+            var itemViewModel = (BaseMovementItemViewModel)item;
+
+            return itemViewModel.CreateTemplate();
         }
     }
 }
