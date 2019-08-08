6d4299b1642a376d3228333350d94ccb90f88d57
alice.le <alice.le@nexcel.vn>
2019-08-08 09:55:31 +0700

    118135- Update icons

--- src/Platforms/LiveScore.Android/Resources/Resource.designer.cs
+++ src/Platforms/LiveScore.Android/Resources/Resource.designer.cs
@@ -8957,26 +8957,26 @@ namespace LiveScore.Droid
 			// aapt resource value: 0x7f020057
 			public const int avd_hide_password = 2130837591;
 			
+			// aapt resource value: 0x7f02018e
+			public const int avd_hide_password_1 = 2130837902;
+			
+			// aapt resource value: 0x7f02018f
+			public const int avd_hide_password_2 = 2130837903;
+			
 			// aapt resource value: 0x7f020190
-			public const int avd_hide_password_1 = 2130837904;
-			
-			// aapt resource value: 0x7f020191
-			public const int avd_hide_password_2 = 2130837905;
-			
-			// aapt resource value: 0x7f020192
-			public const int avd_hide_password_3 = 2130837906;
+			public const int avd_hide_password_3 = 2130837904;
 			
 			// aapt resource value: 0x7f020058
 			public const int avd_show_password = 2130837592;
 			
+			// aapt resource value: 0x7f020191
+			public const int avd_show_password_1 = 2130837905;
+			
+			// aapt resource value: 0x7f020192
+			public const int avd_show_password_2 = 2130837906;
+			
 			// aapt resource value: 0x7f020193
-			public const int avd_show_password_1 = 2130837907;
-			
-			// aapt resource value: 0x7f020194
-			public const int avd_show_password_2 = 2130837908;
-			
-			// aapt resource value: 0x7f020195
-			public const int avd_show_password_3 = 2130837909;
+			public const int avd_show_password_3 = 2130837907;
 			
 			// aapt resource value: 0x7f020059
 			public const int ball = 2130837593;
@@ -9740,11 +9740,11 @@ namespace LiveScore.Droid
 			// aapt resource value: 0x7f020156
 			public const int notification_icon_background = 2130837846;
 			
-			// aapt resource value: 0x7f02018e
-			public const int notification_template_icon_bg = 2130837902;
+			// aapt resource value: 0x7f02018c
+			public const int notification_template_icon_bg = 2130837900;
 			
-			// aapt resource value: 0x7f02018f
-			public const int notification_template_icon_low_bg = 2130837903;
+			// aapt resource value: 0x7f02018d
+			public const int notification_template_icon_low_bg = 2130837901;
 			
 			// aapt resource value: 0x7f020157
 			public const int notification_tile_bg = 2130837847;
@@ -9753,163 +9753,157 @@ namespace LiveScore.Droid
 			public const int notify_panel_notification_icon_bg = 2130837848;
 			
 			// aapt resource value: 0x7f020159
-			public const int odd_asc = 2130837849;
+			public const int odd_movement_chart = 2130837849;
 			
 			// aapt resource value: 0x7f02015a
-			public const int odd_desc = 2130837850;
+			public const int own_goal = 2130837850;
 			
 			// aapt resource value: 0x7f02015b
-			public const int odd_movement_chart = 2130837851;
+			public const int penalty_goal = 2130837851;
 			
 			// aapt resource value: 0x7f02015c
-			public const int own_goal = 2130837852;
+			public const int penalty_winner = 2130837852;
 			
 			// aapt resource value: 0x7f02015d
-			public const int penalty_goal = 2130837853;
+			public const int Prev = 2130837853;
 			
 			// aapt resource value: 0x7f02015e
-			public const int penalty_winner = 2130837854;
+			public const int red_card = 2130837854;
 			
 			// aapt resource value: 0x7f02015f
-			public const int Prev = 2130837855;
+			public const int red_yellow_card = 2130837855;
 			
 			// aapt resource value: 0x7f020160
-			public const int red_card = 2130837856;
+			public const int scores = 2130837856;
 			
 			// aapt resource value: 0x7f020161
-			public const int red_yellow_card = 2130837857;
+			public const int search = 2130837857;
 			
 			// aapt resource value: 0x7f020162
-			public const int scores = 2130837858;
+			public const int second_leg_winner = 2130837858;
 			
 			// aapt resource value: 0x7f020163
-			public const int search = 2130837859;
+			public const int soccer = 2130837859;
 			
 			// aapt resource value: 0x7f020164
-			public const int second_leg_winner = 2130837860;
+			public const int splash_screen = 2130837860;
 			
 			// aapt resource value: 0x7f020165
-			public const int soccer = 2130837861;
+			public const int star = 2130837861;
 			
 			// aapt resource value: 0x7f020166
-			public const int splash_screen = 2130837862;
+			public const int substitution_away_in = 2130837862;
 			
 			// aapt resource value: 0x7f020167
-			public const int star = 2130837863;
+			public const int substitution_away_out = 2130837863;
 			
 			// aapt resource value: 0x7f020168
-			public const int substitution_away_in = 2130837864;
+			public const int substitution_home_in = 2130837864;
 			
 			// aapt resource value: 0x7f020169
-			public const int substitution_away_out = 2130837865;
+			public const int substitution_home_out = 2130837865;
 			
 			// aapt resource value: 0x7f02016a
-			public const int substitution_home_in = 2130837866;
+			public const int tab_feed = 2130837866;
 			
 			// aapt resource value: 0x7f02016b
-			public const int substitution_home_out = 2130837867;
+			public const int tab_h2h = 2130837867;
 			
 			// aapt resource value: 0x7f02016c
-			public const int tab_feed = 2130837868;
+			public const int tab_h2h_active = 2130837868;
 			
 			// aapt resource value: 0x7f02016d
-			public const int tab_h2h = 2130837869;
+			public const int tab_icon = 2130837869;
 			
 			// aapt resource value: 0x7f02016e
-			public const int tab_h2h_active = 2130837870;
+			public const int tab_info = 2130837870;
 			
 			// aapt resource value: 0x7f02016f
-			public const int tab_icon = 2130837871;
+			public const int tab_info_active = 2130837871;
 			
 			// aapt resource value: 0x7f020170
-			public const int tab_info = 2130837872;
+			public const int tab_lineup = 2130837872;
 			
 			// aapt resource value: 0x7f020171
-			public const int tab_info_active = 2130837873;
+			public const int tab_lineup_active = 2130837873;
 			
 			// aapt resource value: 0x7f020172
-			public const int tab_lineup = 2130837874;
+			public const int tab_odds = 2130837874;
 			
 			// aapt resource value: 0x7f020173
-			public const int tab_lineup_active = 2130837875;
+			public const int tab_odds_active = 2130837875;
 			
 			// aapt resource value: 0x7f020174
-			public const int tab_odds = 2130837876;
+			public const int tab_social = 2130837876;
 			
 			// aapt resource value: 0x7f020175
-			public const int tab_odds_active = 2130837877;
+			public const int tab_social_active = 2130837877;
 			
 			// aapt resource value: 0x7f020176
-			public const int tab_social = 2130837878;
+			public const int tab_stats = 2130837878;
 			
 			// aapt resource value: 0x7f020177
-			public const int tab_social_active = 2130837879;
+			public const int tab_stats_active = 2130837879;
 			
 			// aapt resource value: 0x7f020178
-			public const int tab_stats = 2130837880;
+			public const int tab_table = 2130837880;
 			
 			// aapt resource value: 0x7f020179
-			public const int tab_stats_active = 2130837881;
+			public const int tab_table_active = 2130837881;
 			
 			// aapt resource value: 0x7f02017a
-			public const int tab_table = 2130837882;
+			public const int tab_tracker = 2130837882;
 			
 			// aapt resource value: 0x7f02017b
-			public const int tab_table_active = 2130837883;
+			public const int tab_tracker_active = 2130837883;
 			
 			// aapt resource value: 0x7f02017c
-			public const int tab_tracker = 2130837884;
+			public const int tab_tv = 2130837884;
 			
 			// aapt resource value: 0x7f02017d
-			public const int tab_tracker_active = 2130837885;
+			public const int tab_tv_active = 2130837885;
 			
 			// aapt resource value: 0x7f02017e
-			public const int tab_tv = 2130837886;
+			public const int tooltip_frame_dark = 2130837886;
 			
 			// aapt resource value: 0x7f02017f
-			public const int tab_tv_active = 2130837887;
+			public const int tooltip_frame_light = 2130837887;
 			
 			// aapt resource value: 0x7f020180
-			public const int tooltip_frame_dark = 2130837888;
+			public const int tracker = 2130837888;
 			
 			// aapt resource value: 0x7f020181
-			public const int tooltip_frame_light = 2130837889;
+			public const int tv = 2130837889;
 			
 			// aapt resource value: 0x7f020182
-			public const int tracker = 2130837890;
+			public const int uefa_nation_league = 2130837890;
 			
 			// aapt resource value: 0x7f020183
-			public const int tv = 2130837891;
+			public const int uefa_woman_champion_league = 2130837891;
 			
 			// aapt resource value: 0x7f020184
-			public const int uefa_nation_league = 2130837892;
+			public const int uefa_youth_league = 2130837892;
 			
 			// aapt resource value: 0x7f020185
-			public const int uefa_woman_champion_league = 2130837893;
+			public const int Up = 2130837893;
 			
 			// aapt resource value: 0x7f020186
-			public const int uefa_youth_league = 2130837894;
+			public const int user1 = 2130837894;
 			
 			// aapt resource value: 0x7f020187
-			public const int Up = 2130837895;
+			public const int user2 = 2130837895;
 			
 			// aapt resource value: 0x7f020188
-			public const int user1 = 2130837896;
+			public const int user3 = 2130837896;
 			
 			// aapt resource value: 0x7f020189
-			public const int user2 = 2130837897;
+			public const int user4 = 2130837897;
 			
 			// aapt resource value: 0x7f02018a
-			public const int user3 = 2130837898;
+			public const int world_cup = 2130837898;
 			
 			// aapt resource value: 0x7f02018b
-			public const int user4 = 2130837899;
-			
-			// aapt resource value: 0x7f02018c
-			public const int world_cup = 2130837900;
-			
-			// aapt resource value: 0x7f02018d
-			public const int yellow_card = 2130837901;
+			public const int yellow_card = 2130837899;
 			
 			static Drawable()
 			{
