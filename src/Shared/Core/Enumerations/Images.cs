a8e8f74a9c339da81db0736c77b31786943e226c
Vivian <vivian.nguyen@starixsoft.com>
2019-08-08 13:46:44 +0700

    117905 - update penalty goal

--- src/Shared/Core/Enumerations/Images.cs
+++ src/Shared/Core/Enumerations/Images.cs
@@ -8,7 +8,7 @@
         public static readonly Images MissPenaltyGoal = new Images("images/common/missed_penalty_goal.png", nameof(MissPenaltyGoal));
         public static readonly Images OwnGoal = new Images("images/common/own_goal.png", nameof(OwnGoal));
         public static readonly Images Goal = new Images("images/common/ball.png", nameof(Goal));
-        public static readonly Images PenaltyGoal = new Images("images/common/penalty_goal.png", nameof(PenaltyGoal));
+        public static readonly Images PenaltyGoal = new Images("images/common/ball.png", nameof(PenaltyGoal));
         public static readonly Images SecondLeg = new Images("images/common/second_leg_winner.png", nameof(SecondLeg));
         public static readonly Images PenaltyWinner = new Images("images/common/penalty_winner.png", nameof(PenaltyWinner));
 
