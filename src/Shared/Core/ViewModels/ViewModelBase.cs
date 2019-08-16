3f325e195901d8e1db3571729d078b7b34a711db
ricky.nguyen <ricky.nguyen@starixsoft.com>
2019-08-16 10:54:39 +0700

    118356 - Add timer

--- src/Shared/Core/ViewModels/ViewModelBase.cs
+++ src/Shared/Core/ViewModels/ViewModelBase.cs
@@ -1,6 +1,8 @@
 ﻿namespace LiveScore.Core.ViewModels
 {
     using System;
+    using System.Diagnostics;
+    using System.Reflection;
     using System.Threading.Tasks;
     using LiveScore.Common.Services;
     using LiveScore.Core.Services;
