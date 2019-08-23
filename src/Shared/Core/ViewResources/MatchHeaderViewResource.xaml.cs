cdc113141de5eaf232f7b0fcc1762cd5cbc9a032
alice.le <alice.le@nexcel.vn>
2019-08-23 15:45:45 +0700

    119116-Fixed XAML rules

---
+++ src/Shared/Core/ViewResources/MatchHeaderViewResource.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace LiveScore.Core.ViewResources
+{
+    using Xamarin.Forms;
+    using Xamarin.Forms.Xaml;
+
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MatchHeaderViewResource : ResourceDictionary
+    {
+        public MatchHeaderViewResource()
+        {
+            InitializeComponent();
+        }
+    }
+}
