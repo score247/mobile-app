814b27c9a135b3abccb15f984c220cffb6698535
Anders <anders.le@starixsoft.com>
2019-09-13 16:03:56 +0700

    add margin bottom to function tab

--- src/Platforms/LiveScore.iOS/Renderers/FontManager.cs
+++ src/Platforms/LiveScore.iOS/Renderers/FontManager.cs
@@ -5,20 +5,13 @@
 
     public static class FontManager
     {
-        /// <summary>
-        /// Gets a <see cref="UIFont"/> object for the standard font with the desired size.
-        /// </summary>
-        /// <returns>UIFont object for use.</returns>
-        /// <param name="fontSize">Font size.</param>
-        public static UIFont GetFont(nfloat fontSize)
+        public static UIFont GetFont(nfloat fontSize) => GetFont(StandardFontName, fontSize);
+
+        public static UIFont GetFont(string fontFamily, nfloat fontSize)
         {
-            return UIFont.FromName(StandardFontName, fontSize);
+            return UIFont.FromName(fontFamily, fontSize);
         }
 
-        /// <summary>
-        /// Gets the iOS font name for the standard font for this application.
-        /// </summary>
-        /// <value>The name of the standard font.</value>
         public static string StandardFontName => new UIKit.UITextField().Font.Name;
     }
 }
--- src/Platforms/LiveScore.iOS/Renderers/MenuTabbedPageRenderer.cs
+++ src/Platforms/LiveScore.iOS/Renderers/MenuTabbedPageRenderer.cs
@@ -10,6 +10,16 @@ namespace LiveScore.iOS.Renderers
 {
     public class MenuTabbedPageRenderer : TabbedRenderer
     {
+        private readonly UIOffset titlePositionOffset = new UIOffset(0, -4);
+        private readonly UIEdgeInsets iconInsets = new UIEdgeInsets(-2, 0, 2, 0);
+        private const float TabBarHeight = 50f;
+
+        private readonly UITextAttributes uiTextAttributes
+            = new UITextAttributes
+            {
+                Font = FontManager.GetFont(nfloat.Parse(App.Current.Resources["FunctionBarFontSize"].ToString()))
+            };
+
         public override void ViewWillAppear(bool animated)
         {
             SetSelectedTabColor();
@@ -17,35 +27,51 @@ namespace LiveScore.iOS.Renderers
             base.ViewWillAppear(animated);
         }
 
-        private void SetSelectedTabColor()
+        private static void SetSelectedTabColor()
         {
             var color = App.Current.Resources["FunctionBarActiveColor"];
 
-            if (color != null)
+            if (color == null)
             {
-                var selectedColor = (Color)color;
-                UIColor selectedTabColor = UIColor.FromRGB((nfloat)selectedColor.R, (nfloat)selectedColor.G, (nfloat)selectedColor.B);
-
-                UITabBar.Appearance.SelectedImageTintColor = selectedTabColor;
-
-                UITabBarItem.Appearance.SetTitleTextAttributes(
-                    new UITextAttributes
-                    {
-                        TextColor = selectedTabColor,
-                    },
-                    UIControlState.Selected);
+                return;
             }
+
+            var selectedColor = (Color)color;
+            var selectedTabColor = UIColor.FromRGB((nfloat)selectedColor.R, (nfloat)selectedColor.G,
+                (nfloat)selectedColor.B);
+
+            UITabBar.Appearance.SelectedImageTintColor = selectedTabColor;
+
+            UITabBarItem.Appearance.SetTitleTextAttributes(
+                new UITextAttributes
+                {
+                    TextColor = selectedTabColor
+                },
+                UIControlState.Selected);
         }
 
         private void UpdateAllTabBarItems()
         {
-            float.TryParse(App.Current.Resources["FunctionBarFontSize"].ToString(), out var tabbedPageFont);
-
             foreach (var controller in ViewControllers)
             {
-                controller.TabBarItem.SetTitleTextAttributes(
-                    new UITextAttributes { Font = FontManager.GetFont(tabbedPageFont) },
-                    UIControlState.Normal);
+                controller.TabBarItem.SetTitleTextAttributes(uiTextAttributes, UIControlState.Normal);
+            }
+        }
+
+        public override void ViewWillLayoutSubviews()
+        {
+            base.ViewWillLayoutSubviews();
+
+            var tabFrame = TabBar.Frame;
+            tabFrame.Height = TabBarHeight;
+            tabFrame.Y = View.Frame.Height - TabBarHeight;
+
+            TabBar.Frame = tabFrame;
+
+            foreach (var vc in ViewControllers)
+            {
+                vc.TabBarItem.TitlePositionAdjustment = titlePositionOffset;
+                vc.TabBarItem.ImageInsets = iconInsets;
             }
         }
     }
