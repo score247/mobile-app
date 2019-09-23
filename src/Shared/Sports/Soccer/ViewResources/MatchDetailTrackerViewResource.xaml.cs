3a92ab42a1d3999a560dfc9ae5364e750f87b996
alice.le <alice.le@nexcel.vn>
2019-09-23 13:28:11 +0700

    120658-Add UI for TrackerTemplate

---
+++ src/Shared/Sports/Soccer/ViewResources/MatchDetailTrackerViewResource.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace LiveScore.Soccer.ViewResources
+{
+    using Xamarin.Forms;
+    using Xamarin.Forms.Xaml;
+
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MatchDetailTrackerViewResource : ResourceDictionary
+    {
+        public MatchDetailTrackerViewResource()
+        {
+            InitializeComponent();
+        }
+    }
+}
