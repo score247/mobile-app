5726500025de083d0b53f396edea401d6308b94a
Vivian <vivian.nguyen@starixsoft.com>
2019-10-01 14:43:38 +0700

    120644 - update web view to fill

--- src/Platforms/LiveScore.iOS/Renderers/TransparentWebViewRenderer.cs
+++ src/Platforms/LiveScore.iOS/Renderers/TransparentWebViewRenderer.cs
@@ -11,7 +11,6 @@ namespace LiveScore.iOS.Renderers
         {
             base.OnElementChanged(e);
             this.BackgroundColor = UIColor.Clear;
-            this.ScalesPageToFit = true;
         }
     }
 }
