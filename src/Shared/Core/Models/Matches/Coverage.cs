b7775bb92646c18c2becfbefeee9bcc87a12492f
ricky.nguyen <ricky.nguyen@starixsoft.com>
2019-10-01 12:24:09 +0700

    121095	- Added Coverage Model

---
+++ src/Shared/Core/Models/Matches/Coverage.cs
@@ -0,0 +1,20 @@
+﻿namespace LiveScore.Core.Models.Matches
+{
+    using MessagePack;
+
+    [MessagePackObject(keyAsPropertyName: true)]
+    public class Coverage
+    {
+        public bool Live { get; set; }
+
+        public bool BasicScore { get; set; }
+
+        public bool KeyEvents { get; set; }
+
+        public bool DetailedEvents { get; set; }
+
+        public bool Lineups { get; set; }
+
+        public bool Commentary { get; set; }
+    }
+}
---
+++ src/Shared/Sports/Soccer/Models/Matches/MatchCoverage.cs
@@ -0,0 +1,15 @@
+﻿namespace LiveScore.Soccer.Models.Matches
+{
+    using LiveScore.Core.Models.Matches;
+    using MessagePack;
+
+    [MessagePackObject]    
+    public class MatchCoverage
+    {
+        [Key(0)]
+        public string MatchId { get; set; }
+
+        [Key(1)]
+        public Coverage Coverage { get; set; }
+    }
+}
