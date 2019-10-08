31565c95c19556a6a8d48b0f7ca8eaa89c0a559e
alice.le <alice.le@nexcel.vn>
2019-10-08 17:13:53 +0700

    121628-Update UI  for score page

---
+++ src/LiveScore.Features/Score/ViewResources/ScoreViewResource.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace LiveScore.Features.Score.ViewResources
+{
+    using Xamarin.Forms;
+    using Xamarin.Forms.Xaml;
+
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ScoreViewResource : ResourceDictionary
+    {
+        public ScoreViewResource()
+        {
+            InitializeComponent();
+        }
+    }
+}
