f6b3fdfd7724f324ba94c027ced832fb890e7735
ricky.nguyen <ricky.nguyen@starixsoft.com>
2019-10-11 16:52:46 +0700

    121694 - add custom progress bar for possession

---
+++ src/Platforms/LiveScore.iOS/Renderers/PossessionProgressBarRenderer.cs
@@ -0,0 +1,30 @@
+﻿using CoreGraphics;
+using LiveScore.Core.Controls.ProgressBars;
+using LiveScore.iOS.Renderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(PossessionProgressBar), typeof(PossessionProgressBarRenderer))]
+
+namespace LiveScore.iOS.Renderers
+{
+    public class PossessionProgressBarRenderer : ProgressBarRenderer
+    {
+        protected override void OnElementChanged(
+         ElementChangedEventArgs<Xamarin.Forms.ProgressBar> e)
+        {
+            base.OnElementChanged(e);
+        }
+
+        public override void LayoutSubviews()
+        {
+            base.LayoutSubviews();
+
+            var X = 1.0f;
+            var Y = 5.0f; // This changes the height
+
+            CGAffineTransform transform = CGAffineTransform.MakeScale(X, Y);
+            Control.Transform = transform;
+        }
+    }
+}
---
+++ src/Shared/Core/Controls/ProgressBars/PossessionProgressBar.cs
@@ -0,0 +1,8 @@
+﻿using Xamarin.Forms;
+
+namespace LiveScore.Core.Controls.ProgressBars
+{
+    public class PossessionProgressBar : ProgressBar
+    {
+    }
+}
