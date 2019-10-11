c86215c596f4dc72251334e727c2a6e6af876b84
Vivian <vivian.nguyen@starixsoft.com>
2019-10-11 18:43:30 +0700

    000 - update configuration

--- src/LiveScore/Configurations/Configuration.cs
+++ src/LiveScore/Configurations/Configuration.cs
@@ -5,23 +5,23 @@
 #if DEBUG
         public static string ApiEndPoint => "https://score247-api3.nexdev.net/dev/api";
         public static string SignalRHubEndPoint => "https://score247-api4.nexdev.net/dev/hubs";
-        public static string AssetsEndPoint => "https://assets-dev.nexdev.net/main";
+        public static string AssetsEndPoint => "https://assets-dev.nexdev.net/test/";
         public static string Environment => "DEV";
 #elif TEST
         public static string ApiEndPoint => "https://score247-api3.nexdev.net/test/api";
         public static string SignalRHubEndPoint => "https://score247-api4.nexdev.net/test/hubs";
-        public static string AssetsEndPoint => "https://assets-dev.nexdev.net/test";
+        public static string AssetsEndPoint => "https://assets-dev.nexdev.net/test/";
         public static string Environment => "TEST";
 #elif DEVRELEASE
         public static string ApiEndPoint => "https://score247-api3.nexdev.net/main/api";
         public static string SignalRHubEndPoint => "https://score247-api4.nexdev.net/main/hubs";
-        public static string AssetsEndPoint => "https://assets-dev.nexdev.net/test";
+        public static string AssetsEndPoint => "https://assets-dev.nexdev.net/test/";
         public static string Environment => "DEV-RELEASE";
 #else
 
         public static string ApiEndPoint => "https://score247-api3.nexdev.net/main/api";
         public static string SignalRHubEndPoint => "https://score247-api4.nexdev.net/main/hubs";
-        public static string AssetsEndPoint => "https://assets-dev.nexdev.net/main";
+        public static string AssetsEndPoint => "https://assets-dev.nexdev.net/main/";
         public static string Environment => "MAIN";
 #endif
 
