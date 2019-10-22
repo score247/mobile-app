2c1be3f79142672e41dd37c0e191d0009c4d2135
alice.le <alice.le@nexcel.vn>
2019-10-22 13:09:50 +0700

    122477-Implement UI for head to head and update icon svg for timeline events

---
+++ src/Shared/Sports/Soccer/ViewResources/MatchDetailH2HViewResource.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace LiveScore.Soccer.ViewResources
+{
+    using Xamarin.Forms;
+    using Xamarin.Forms.Xaml;
+
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MatchDetailH2HViewResource : ResourceDictionary
+    {
+        public MatchDetailH2HViewResource()
+        {
+            InitializeComponent();
+        }
+    }
+}
