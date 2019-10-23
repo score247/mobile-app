08da5cc366d491ae203d78cbe4151cea8fe10258
Vivian.Nguyen <vivian.nguyen@nexcel.vn>
2019-10-23 15:11:00 +0700

    000 - update test prj

--- src/Shared/Sports/Soccer.Tests/Services/MatchServiceTests.cs
+++ src/Shared/Sports/Soccer.Tests/Services/MatchServiceTests.cs
@@ -1,129 +1,129 @@
-﻿namespace Soccer.Tests.Services
-{
-    using LiveScore.Core.Tests.Fixtures;
-    using Xunit;
+﻿//namespace Soccer.Tests.Services
+//{
+//    using LiveScore.Core.Tests.Fixtures;
+//    using Xunit;
 
-    public class MatchServiceTests : IClassFixture<CommonFixture>
-    {
-        //    private readonly CompareLogic comparer;
-        //    private readonly Fixture fixture;
-        //    private readonly IApiService apiService;
-        //    private readonly ICachingService cacheService;
-        //    private readonly ILoggingService loggingService;
-        //    private readonly IMatchService matchService;
+//    //public class MatchServiceTests : IClassFixture<CommonFixture>
+//    //{
+//        //    private readonly CompareLogic comparer;
+//        //    private readonly Fixture fixture;
+//        //    private readonly IApiService apiService;
+//        //    private readonly ICachingService cacheService;
+//        //    private readonly ILoggingService loggingService;
+//        //    private readonly IMatchService matchService;
 
-        //    public MatchServiceTests(CommonFixture commonFixture)
-        //    {
-        //        comparer = commonFixture.Comparer;
-        //        fixture = commonFixture.Specimens;
-        //        apiService = Substitute.For<IApiService>();
-        //        cacheService = Substitute.For<ICachingService>();
-        //        loggingService = Substitute.For<ILoggingService>();
+//        //    public MatchServiceTests(CommonFixture commonFixture)
+//        //    {
+//        //        comparer = commonFixture.Comparer;
+//        //        fixture = commonFixture.Specimens;
+//        //        apiService = Substitute.For<IApiService>();
+//        //        cacheService = Substitute.For<ICachingService>();
+//        //        loggingService = Substitute.For<ILoggingService>();
 
 
-        //        matchService = new MatchService(apiService, cacheService, loggingService);
-        //    }
+//        //        matchService = new MatchService(apiService, cacheService, loggingService);
+//        //    }
 
-        //    [Fact]
-        //    public async Task GetMatches_WhenCall_InjectCacheService()
-        //    {
-        //        // Arrange
-        //        var dateTime = DateTime.Now;
-        //        var dateRange = new DateRange(dateTime);
+//        //    [Fact]
+//        //    public async Task GetMatches_WhenCall_InjectCacheService()
+//        //    {
+//        //        // Arrange
+//        //        var dateTime = DateTime.Now;
+//        //        var dateRange = new DateRange(dateTime);
 
-        //        // Act
-        //        await matchService.GetMatches(dateRange, Language.English);
+//        //        // Act
+//        //        await matchService.GetMatches(dateRange, Language.English);
 
-        //        // Assert
-        //        await cacheService.Received(1)
-        //            .GetAndFetchLatestLocalMachine(
-        //                Arg.Any<string>(),
-        //                Arg.Any<Func<Task<IEnumerable<SoccerMatch>>>>(),
-        //                Arg.Any<Func<DateTimeOffset, bool>>(),
-        //                null);
-        //    }
+//        //        // Assert
+//        //        await cacheService.Received(1)
+//        //            .GetAndFetchLatestLocalMachine(
+//        //                Arg.Any<string>(),
+//        //                Arg.Any<Func<Task<IEnumerable<SoccerMatch>>>>(),
+//        //                Arg.Any<Func<DateTimeOffset, bool>>(),
+//        //                null);
+//        //    }
 
-        //    [Fact]
-        //    public async Task GetMatches_ThrowsException_InjectLoggingServiceAndReturnEmptyList()
-        //    {
-        //        // Arrange
-        //        var dateTime = DateTime.Now;
-        //        var dateRange = new DateRange(dateTime);
-        //        cacheService.GetAndFetchLatestLocalMachine(
-        //                Arg.Any<string>(),
-        //                Arg.Any<Func<Task<IEnumerable<SoccerMatch>>>>(),
-        //                Arg.Any<Func<DateTimeOffset, bool>>(),
-        //                Arg.Any<DateTimeOffset>())
-        //            .ThrowsForAnyArgs(new InvalidOperationException("NotFound Key"));
+//        //    [Fact]
+//        //    public async Task GetMatches_ThrowsException_InjectLoggingServiceAndReturnEmptyList()
+//        //    {
+//        //        // Arrange
+//        //        var dateTime = DateTime.Now;
+//        //        var dateRange = new DateRange(dateTime);
+//        //        cacheService.GetAndFetchLatestLocalMachine(
+//        //                Arg.Any<string>(),
+//        //                Arg.Any<Func<Task<IEnumerable<SoccerMatch>>>>(),
+//        //                Arg.Any<Func<DateTimeOffset, bool>>(),
+//        //                Arg.Any<DateTimeOffset>())
+//        //            .ThrowsForAnyArgs(new InvalidOperationException("NotFound Key"));
 
-        //        // Act
-        //        var matches = await matchService.GetMatches(dateRange, Language.English);
+//        //        // Act
+//        //        var matches = await matchService.GetMatches(dateRange, Language.English);
 
-        //        // Assert
-        //        loggingService.Received(1).LogError(Arg.Any<InvalidOperationException>());
-        //        Assert.Empty(matches);
-        //    }
+//        //        // Assert
+//        //        loggingService.Received(1).LogError(Arg.Any<InvalidOperationException>());
+//        //        Assert.Empty(matches);
+//        //    }
 
-        //    [Fact]
-        //    public async Task GetMatches_CacheHasValue_ShouldReturnCorrectListCountFromCache()
-        //    {
-        //        // Arrange
-        //        var dateTime = DateTime.Now;
-        //        var dateRange = new DateRange(dateTime);
-        //        var cacheKey = $"Matches:{dateTime.Date}:en-US";
+//        //    [Fact]
+//        //    public async Task GetMatches_CacheHasValue_ShouldReturnCorrectListCountFromCache()
+//        //    {
+//        //        // Arrange
+//        //        var dateTime = DateTime.Now;
+//        //        var dateRange = new DateRange(dateTime);
+//        //        var cacheKey = $"Matches:{dateTime.Date}:en-US";
 
-        //        var expectedMatches = fixture.CreateMany<SoccerMatch>();
+//        //        var expectedMatches = fixture.CreateMany<SoccerMatch>();
 
-        //        cacheService.GetAndFetchLatestLocalMachine(
-        //            cacheKey,
-        //            Arg.Any<Func<Task<IEnumerable<SoccerMatch>>>>(),
-        //            Arg.Any<Func<DateTimeOffset, bool>>(),
-        //            null).Returns(expectedMatches);
+//        //        cacheService.GetAndFetchLatestLocalMachine(
+//        //            cacheKey,
+//        //            Arg.Any<Func<Task<IEnumerable<SoccerMatch>>>>(),
+//        //            Arg.Any<Func<DateTimeOffset, bool>>(),
+//        //            null).Returns(expectedMatches);
 
-        //        // Act
-        //        var actualMatches = await matchService.GetMatches(dateRange, Language.English, true);
+//        //        // Act
+//        //        var actualMatches = await matchService.GetMatches(dateRange, Language.English, true);
 
-        //        // Assert
-        //        Assert.True(comparer.Compare(expectedMatches, actualMatches).AreEqual);
-        //    }
+//        //        // Assert
+//        //        Assert.True(comparer.Compare(expectedMatches, actualMatches).AreEqual);
+//        //    }
 
-        //    [Fact]
-        //    public async Task GetMatch_CacheHasValue_ReturnDataFromCache()
-        //    {
-        //        // Arrange
-        //        var matchId = new Fixture().Create<string>();
-        //        var cacheKey = $"SoccerMatch:{matchId}:en-US";
-        //        var expectedMatch = fixture.Create<SoccerMatch>();
+//        //    [Fact]
+//        //    public async Task GetMatch_CacheHasValue_ReturnDataFromCache()
+//        //    {
+//        //        // Arrange
+//        //        var matchId = new Fixture().Create<string>();
+//        //        var cacheKey = $"SoccerMatch:{matchId}:en-US";
+//        //        var expectedMatch = fixture.Create<SoccerMatch>();
 
-        //        cacheService.GetAndFetchLatestLocalMachine(
-        //           cacheKey,
-        //           Arg.Any<Func<Task<SoccerMatch>>>(),
-        //           Arg.Any<Func<DateTimeOffset, bool>>(),
-        //           null).Returns(expectedMatch);
+//        //        cacheService.GetAndFetchLatestLocalMachine(
+//        //           cacheKey,
+//        //           Arg.Any<Func<Task<SoccerMatch>>>(),
+//        //           Arg.Any<Func<DateTimeOffset, bool>>(),
+//        //           null).Returns(expectedMatch);
 
-        //        // Act
-        //        var actualMatch = await matchService.GetMatch(matchId, Language.English, false);
+//        //        // Act
+//        //        var actualMatch = await matchService.GetMatch(matchId, Language.English, false);
 
-        //        // Assert
-        //        Assert.Equal(expectedMatch, actualMatch);
-        //    }
+//        //        // Assert
+//        //        Assert.Equal(expectedMatch, actualMatch);
+//        //    }
 
-        //    [Fact]
-        //    public async Task GetMatch_Exception_WriteLog()
-        //    {
-        //        // Arrange
-        //        cacheService.GetAndFetchLatestLocalMachine(
-        //           "GetMatch:123:en-US",
-        //           Arg.Any<Func<Task<SoccerMatch>>>(),
-        //           Arg.Any<Func<DateTimeOffset, bool>>(),
-        //           null).ThrowsForAnyArgs(new InvalidOperationException("NotFound Key"));
+//        //    [Fact]
+//        //    public async Task GetMatch_Exception_WriteLog()
+//        //    {
+//        //        // Arrange
+//        //        cacheService.GetAndFetchLatestLocalMachine(
+//        //           "GetMatch:123:en-US",
+//        //           Arg.Any<Func<Task<SoccerMatch>>>(),
+//        //           Arg.Any<Func<DateTimeOffset, bool>>(),
+//        //           null).ThrowsForAnyArgs(new InvalidOperationException("NotFound Key"));
 
-        //        // Act
-        //        var actualMatch = await matchService.GetMatch("123", Language.English, false);
+//        //        // Act
+//        //        var actualMatch = await matchService.GetMatch("123", Language.English, false);
 
-        //        // Assert
-        //        loggingService.Received(1).LogError(Arg.Any<InvalidOperationException>());
-        //        Assert.Null(actualMatch);
-        //    }
-    }
-}
+//        //        // Assert
+//        //        loggingService.Received(1).LogError(Arg.Any<InvalidOperationException>());
+//        //        Assert.Null(actualMatch);
+//        //    }
+//    //}
+//}
--- src/Shared/Sports/Soccer.Tests/Services/OddsServiceTests.cs
+++ src/Shared/Sports/Soccer.Tests/Services/OddsServiceTests.cs
@@ -1,177 +1,177 @@
-﻿namespace Soccer.Tests.Services
-{
-    //using System;
-    //using System.Threading.Tasks;
-    //using AutoFixture;
-    //using KellermanSoftware.CompareNetObjects;
-    //using LiveScore.Common.Services;
-    //using LiveScore.Core.Services;
-    //using LiveScore.Core.Tests.Fixtures;
-    //using LiveScore.Soccer.Models.Odds;
-    //using LiveScore.Soccer.Services;
-    //using NSubstitute;
-    //using NSubstitute.ExceptionExtensions;
-    //using Xunit;
+﻿//namespace Soccer.Tests.Services
+//{
+//    //using System;
+//    //using System.Threading.Tasks;
+//    //using AutoFixture;
+//    //using KellermanSoftware.CompareNetObjects;
+//    //using LiveScore.Common.Services;
+//    //using LiveScore.Core.Services;
+//    //using LiveScore.Core.Tests.Fixtures;
+//    //using LiveScore.Soccer.Models.Odds;
+//    //using LiveScore.Soccer.Services;
+//    //using NSubstitute;
+//    //using NSubstitute.ExceptionExtensions;
+//    //using Xunit;
 
-    //public class OddsServiceTests : IClassFixture<CommonFixture>
-    //{
-    //    private readonly CompareLogic comparer;
-    //    private readonly Fixture fixture;
-    //    private readonly IApiService mockApiService;
-    //    private readonly ICachingService mockCache;
-    //    private readonly ILoggingService mockLogger;
-    //    private readonly IOddsService oddsService;
+//    //public class OddsServiceTests : IClassFixture<CommonFixture>
+//    //{
+//    //    private readonly CompareLogic comparer;
+//    //    private readonly Fixture fixture;
+//    //    private readonly IApiService mockApiService;
+//    //    private readonly ICachingService mockCache;
+//    //    private readonly ILoggingService mockLogger;
+//    //    private readonly IOddsService oddsService;
 
-    //    public OddsServiceTests(CommonFixture commonFixture)
-    //    {
-    //        comparer = commonFixture.Comparer;
-    //        fixture = commonFixture.Specimens;
-    //        mockCache = Substitute.For<ICachingService>();
-    //        mockLogger = Substitute.For<ILoggingService>();
-    //        mockApiService = Substitute.For<IApiService>();
+//    //    public OddsServiceTests(CommonFixture commonFixture)
+//    //    {
+//    //        comparer = commonFixture.Comparer;
+//    //        fixture = commonFixture.Specimens;
+//    //        mockCache = Substitute.For<ICachingService>();
+//    //        mockLogger = Substitute.For<ILoggingService>();
+//    //        mockApiService = Substitute.For<IApiService>();
 
-    //        oddsService = new OddsService(mockCache, mockLogger, mockApiService);
-    //    }
+//    //        oddsService = new OddsService(mockCache, mockLogger, mockApiService);
+//    //    }
 
-    //    [Fact]
-    //    public async Task GetOdds_ForceFetchNew_GetAndFetchLatestValue()
-    //    {
-    //        // Arrange
+//    //    [Fact]
+//    //    public async Task GetOdds_ForceFetchNew_GetAndFetchLatestValue()
+//    //    {
+//    //        // Arrange
 
-    //        // Act
-    //        await oddsService.GetOdds("en", "sr:match:1", 1, "decimal", forceFetchNewData: true);
+//    //        // Act
+//    //        await oddsService.GetOdds("en", "sr:match:1", 1, "decimal", forceFetchNewData: true);
 
-    //        // Assert
-    //        await mockCache.Received(1)
-    //            .GetAndFetchLatestLocalMachine(
-    //                Arg.Any<string>(),
-    //                Arg.Any<Func<Task<MatchOdds>>>(),
-    //                Arg.Any<Func<DateTimeOffset, bool>>(),
-    //                null);
-    //    }
+//    //        // Assert
+//    //        await mockCache.Received(1)
+//    //            .GetAndFetchLatestLocalMachine(
+//    //                Arg.Any<string>(),
+//    //                Arg.Any<Func<Task<MatchOdds>>>(),
+//    //                Arg.Any<Func<DateTimeOffset, bool>>(),
+//    //                null);
+//    //    }
 
-    //    [Fact]
-    //    public async Task GetOdds_NotForceFetchNew_InjectCacheService()
-    //    {
-    //        // Arrange
+//    //    [Fact]
+//    //    public async Task GetOdds_NotForceFetchNew_InjectCacheService()
+//    //    {
+//    //        // Arrange
 
-    //        // Act
-    //        await oddsService.GetOdds("en", "sr:match:1", 1, "decimal", forceFetchNewData: false);
+//    //        // Act
+//    //        await oddsService.GetOdds("en", "sr:match:1", 1, "decimal", forceFetchNewData: false);
 
-    //        // Assert
-    //        await mockCache.Received(1)
-    //            .GetOrFetchLocalMachine(
-    //                Arg.Any<string>(),
-    //                Arg.Any<Func<Task<MatchOdds>>>(),
-    //                Arg.Any<DateTimeOffset>());
-    //    }
+//    //        // Assert
+//    //        await mockCache.Received(1)
+//    //            .GetOrFetchLocalMachine(
+//    //                Arg.Any<string>(),
+//    //                Arg.Any<Func<Task<MatchOdds>>>(),
+//    //                Arg.Any<DateTimeOffset>());
+//    //    }
 
-    //    [Fact]
-    //    public async Task GetOdds_ThrowsException_InjectLoggingServiceAndReturnEmptyList()
-    //    {
-    //        // Arrange
-    //        mockCache.GetAndFetchLatestLocalMachine(
-    //                Arg.Any<string>(),
-    //                Arg.Any<Func<Task<MatchOdds>>>(),
-    //                Arg.Any<Func<DateTimeOffset, bool>>(),
-    //                Arg.Any<DateTimeOffset>())
-    //            .ThrowsForAnyArgs(new InvalidOperationException("NotFound Key"));
+//    //    [Fact]
+//    //    public async Task GetOdds_ThrowsException_InjectLoggingServiceAndReturnEmptyList()
+//    //    {
+//    //        // Arrange
+//    //        mockCache.GetAndFetchLatestLocalMachine(
+//    //                Arg.Any<string>(),
+//    //                Arg.Any<Func<Task<MatchOdds>>>(),
+//    //                Arg.Any<Func<DateTimeOffset, bool>>(),
+//    //                Arg.Any<DateTimeOffset>())
+//    //            .ThrowsForAnyArgs(new InvalidOperationException("NotFound Key"));
 
-    //        // Act
-    //        var odds = await oddsService.GetOdds("en", "sr:match:1", 1, "decimal", forceFetchNewData: true);
+//    //        // Act
+//    //        var odds = await oddsService.GetOdds("en", "sr:match:1", 1, "decimal", forceFetchNewData: true);
 
-    //        // Assert
-    //        mockLogger.Received(1).LogError(Arg.Any<InvalidOperationException>());
-    //        Assert.NotNull(odds);
-    //    }
+//    //        // Assert
+//    //        mockLogger.Received(1).LogError(Arg.Any<InvalidOperationException>());
+//    //        Assert.NotNull(odds);
+//    //    }
 
-    //    [Fact]
-    //    public async Task GetOdds_CacheHasValue_ShouldReturnCorrectListCountFromCache()
-    //    {
-    //        // Arrange
-    //        var expectedMatchOdds = fixture.Create<MatchOdds>();
+//    //    [Fact]
+//    //    public async Task GetOdds_CacheHasValue_ShouldReturnCorrectListCountFromCache()
+//    //    {
+//    //        // Arrange
+//    //        var expectedMatchOdds = fixture.Create<MatchOdds>();
 
-    //        mockCache.GetOrFetchLocalMachine(
-    //            "OddsComparison-sr:match:1-1-decimal",
-    //            Arg.Any<Func<Task<MatchOdds>>>(),
-    //            Arg.Any<DateTimeOffset>()).Returns(expectedMatchOdds);
+//    //        mockCache.GetOrFetchLocalMachine(
+//    //            "OddsComparison-sr:match:1-1-decimal",
+//    //            Arg.Any<Func<Task<MatchOdds>>>(),
+//    //            Arg.Any<DateTimeOffset>()).Returns(expectedMatchOdds);
 
-    //        // Act
-    //        var actualOdds = await oddsService.GetOdds("en", "sr:match:1", 1, "decimal");
+//    //        // Act
+//    //        var actualOdds = await oddsService.GetOdds("en", "sr:match:1", 1, "decimal");
 
-    //        // Assert
-    //        Assert.True(comparer.Compare(expectedMatchOdds, actualOdds).AreEqual);
-    //    }
+//    //        // Assert
+//    //        Assert.True(comparer.Compare(expectedMatchOdds, actualOdds).AreEqual);
+//    //    }
 
-    //    [Fact]
-    //    public async Task GetOddsMovement_ForceFetchNew_GetAndFetchLatestValue()
-    //    {
-    //        // Arrange
+//    //    [Fact]
+//    //    public async Task GetOddsMovement_ForceFetchNew_GetAndFetchLatestValue()
+//    //    {
+//    //        // Arrange
 
-    //        // Act
-    //        await oddsService.GetOddsMovement("en", "sr:match:1", 1, "dec", "sr:book:1", forceFetchNewData: true);
+//    //        // Act
+//    //        await oddsService.GetOddsMovement("en", "sr:match:1", 1, "dec", "sr:book:1", forceFetchNewData: true);
 
-    //        // Assert
-    //        await mockCache.Received(1)
-    //            .GetAndFetchLatestLocalMachine(
-    //                Arg.Any<string>(),
-    //                Arg.Any<Func<Task<MatchOddsMovement>>>(),
-    //                Arg.Any<Func<DateTimeOffset, bool>>(),
-    //                null);
-    //    }
+//    //        // Assert
+//    //        await mockCache.Received(1)
+//    //            .GetAndFetchLatestLocalMachine(
+//    //                Arg.Any<string>(),
+//    //                Arg.Any<Func<Task<MatchOddsMovement>>>(),
+//    //                Arg.Any<Func<DateTimeOffset, bool>>(),
+//    //                null);
+//    //    }
 
-    //    [Fact]
-    //    public async Task GetOddsMovement_ForceFetchNew_GetOrFetchValue()
-    //    {
-    //        // Arrange
+//    //    [Fact]
+//    //    public async Task GetOddsMovement_ForceFetchNew_GetOrFetchValue()
+//    //    {
+//    //        // Arrange
 
-    //        // Act
-    //        await oddsService.GetOddsMovement("en", "sr:match:1", 1, "dec", "sr:book:1", forceFetchNewData: false);
+//    //        // Act
+//    //        await oddsService.GetOddsMovement("en", "sr:match:1", 1, "dec", "sr:book:1", forceFetchNewData: false);
 
-    //        // Assert
-    //        await mockCache.Received(1)
-    //            .GetOrFetchLocalMachine(
-    //                Arg.Any<string>(),
-    //                Arg.Any<Func<Task<MatchOddsMovement>>>(),
-    //                Arg.Any<DateTimeOffset>());
-    //    }
+//    //        // Assert
+//    //        await mockCache.Received(1)
+//    //            .GetOrFetchLocalMachine(
+//    //                Arg.Any<string>(),
+//    //                Arg.Any<Func<Task<MatchOddsMovement>>>(),
+//    //                Arg.Any<DateTimeOffset>());
+//    //    }
 
-    //    [Fact]
-    //    public async Task GetOddsMovement_ThrowsException_InjectLoggingServiceAndReturnEmptyList()
-    //    {
-    //        // Arrange
-    //        mockCache.GetAndFetchLatestLocalMachine(
-    //                Arg.Any<string>(),
-    //                Arg.Any<Func<Task<MatchOddsMovement>>>(),
-    //                Arg.Any<Func<DateTimeOffset, bool>>(),
-    //                Arg.Any<DateTimeOffset>())
-    //            .ThrowsForAnyArgs(new InvalidOperationException("NotFound Key"));
+//    //    [Fact]
+//    //    public async Task GetOddsMovement_ThrowsException_InjectLoggingServiceAndReturnEmptyList()
+//    //    {
+//    //        // Arrange
+//    //        mockCache.GetAndFetchLatestLocalMachine(
+//    //                Arg.Any<string>(),
+//    //                Arg.Any<Func<Task<MatchOddsMovement>>>(),
+//    //                Arg.Any<Func<DateTimeOffset, bool>>(),
+//    //                Arg.Any<DateTimeOffset>())
+//    //            .ThrowsForAnyArgs(new InvalidOperationException("NotFound Key"));
 
-    //        // Act
-    //        var odds = await oddsService.GetOddsMovement("en", "sr:match:1", 1, "dec", "sr:book:1", true);
+//    //        // Act
+//    //        var odds = await oddsService.GetOddsMovement("en", "sr:match:1", 1, "dec", "sr:book:1", true);
 
-    //        // Assert
-    //        mockLogger.Received(1).LogError(Arg.Any<InvalidOperationException>());
-    //        Assert.NotNull(odds);
-    //    }
+//    //        // Assert
+//    //        mockLogger.Received(1).LogError(Arg.Any<InvalidOperationException>());
+//    //        Assert.NotNull(odds);
+//    //    }
 
-    //    [Fact]
-    //    public async Task GetOddsMovement_CacheHasValue_ShouldReturnCorrectListCountFromCache()
-    //    {
-    //        // Arrange
-    //        var expectedMatchOdds = fixture.Create<MatchOddsMovement>();
+//    //    [Fact]
+//    //    public async Task GetOddsMovement_CacheHasValue_ShouldReturnCorrectListCountFromCache()
+//    //    {
+//    //        // Arrange
+//    //        var expectedMatchOdds = fixture.Create<MatchOddsMovement>();
 
-    //        mockCache.GetAndFetchLatestLocalMachine(
-    //            "OddsMovement-sr:match:1-1-dec-sr:book:1",
-    //            Arg.Any<Func<Task<MatchOddsMovement>>>(),
-    //            Arg.Any<Func<DateTimeOffset, bool>>(),
-    //            null).Returns(expectedMatchOdds);
+//    //        mockCache.GetAndFetchLatestLocalMachine(
+//    //            "OddsMovement-sr:match:1-1-dec-sr:book:1",
+//    //            Arg.Any<Func<Task<MatchOddsMovement>>>(),
+//    //            Arg.Any<Func<DateTimeOffset, bool>>(),
+//    //            null).Returns(expectedMatchOdds);
 
-    //        // Act
-    //        var actualOdds = await oddsService.GetOddsMovement("en", "sr:match:1", 1, "dec", "sr:book:1", true);
+//    //        // Act
+//    //        var actualOdds = await oddsService.GetOddsMovement("en", "sr:match:1", 1, "dec", "sr:book:1", true);
 
-    //        // Assert
-    //        Assert.True(comparer.Compare(expectedMatchOdds, actualOdds).AreEqual);
-    //    }
-    //}
-}
+//    //        // Assert
+//    //        Assert.True(comparer.Compare(expectedMatchOdds, actualOdds).AreEqual);
+//    //    }
+//    //}
+//}
