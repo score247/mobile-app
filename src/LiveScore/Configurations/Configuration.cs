d82c1f479bfd808a3418184b59d3232fc0333ff5
alice.le <alice.le@nexcel.vn>
2019-10-23 16:11:26 +0700

    122477-Update  Roboto font

--- src/LiveScore/Configurations/Configuration.cs
+++ src/LiveScore/Configurations/Configuration.cs
@@ -5,11 +5,12 @@ namespace LiveScore.Configurations
     public class Configuration : IConfiguration
     {
 #if DEBUG
-        public string ApiEndPoint => "https://score247-api3.nexdev.net/dev/api";
-        public string SignalRHubEndPoint => "https://score247-api4.nexdev.net/dev/hubs";
-        public string AssetsEndPoint => "https://assets-dev.nexdev.net/test/";
-        public string AppCenterSecret => "ios=b08e2753-b596-44c9-bed9-701d8dd8be8c;";
-        public string Environment => "DEV";
+        public string ApiEndPoint => "https://score247-api3.nexdev.net/main/api";
+        public string SignalRHubEndPoint => "https://score247-api4.nexdev.net/main/hubs";
+        public string AssetsEndPoint => "https://assets-dev.nexdev.net/main/";
+
+        public string AppCenterSecret => "ios=34adf4e9-18dd-4ef0-817f-48bce4ff7159;";
+        public string Environment => "MAIN";
 
         //DataGenerator: http://ha.nexdev.net:7208/dev1
 #elif TEST
