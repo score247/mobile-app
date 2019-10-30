14af8d047880e0da53f4adaf0d0c03df0a337c3a
Harrison Nguyen <harrison.nguyen@starixsoft.com>
2019-10-30 18:35:47 +0700

    Use collection view for Statistics

---
+++ src/Shared/Sports/Soccer/Views/Templates/MatchDetails/DetailStatistics/StatisticsHeaderTemplate.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LiveScore.Soccer.Views.Templates.MatchDetails.DetailStatistics
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class StatisticsHeaderTemplate : DataTemplate
+    {
+        public StatisticsHeaderTemplate()
+        {
+            InitializeComponent();
+        }
+    }
+}
