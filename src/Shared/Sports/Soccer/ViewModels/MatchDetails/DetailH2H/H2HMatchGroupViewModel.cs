2a66c773f9975d70a24e9d1ff0bd7b27f46e58ef
Vivian.Nguyen <vivian.nguyen@nexcel.vn>
2019-10-30 09:15:53 +0700

    122477 - separate H2HMatchGrouping

--- src/Shared/Sports/Soccer/ViewModels/MatchDetails/DetailH2H/H2HMatchGroupViewModel.cs
+++ src/Shared/Sports/Soccer/ViewModels/MatchDetails/DetailH2H/H2HMatchGroupViewModel.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using LiveScore.Core.Models.Matches;
 
 namespace LiveScore.Soccer.ViewModels.MatchDetails.DetailH2H
 {
@@ -19,46 +18,5 @@ namespace LiveScore.Soccer.ViewModels.MatchDetails.DetailH2H
         public string LeagueName { get; }
 
         public string CountryFlag { get; }
-    }
-
-    public class H2HMatchGrouping
-    {
-        public H2HMatchGrouping(IMatch match)
-        {
-            if (match == null)
-            {
-                return;
-            }
-
-            LeagueId = match.LeagueId;
-            LeagueName = match.LeagueGroupName;
-            LeagueSeasonId = match.LeagueSeasonId;
-        }
-
-        public string LeagueId { get; }
-
-        public string LeagueName { get; }
-
-        public string LeagueSeasonId { get; }
-
-        public override bool Equals(object obj)
-            => (obj is H2HMatchGrouping actualObj)
-                && LeagueId == actualObj.LeagueId
-                && LeagueSeasonId == actualObj.LeagueSeasonId;
-
-        public override int GetHashCode()
-        {
-            if (!string.IsNullOrWhiteSpace(LeagueSeasonId))
-            {
-                return LeagueSeasonId.GetHashCode();
-            }
-
-            if (string.IsNullOrWhiteSpace(LeagueId))
-            {
-                return LeagueName?.GetHashCode() ?? 0;
-            }
-
-            return LeagueId.GetHashCode();
-        }
     }
 }
