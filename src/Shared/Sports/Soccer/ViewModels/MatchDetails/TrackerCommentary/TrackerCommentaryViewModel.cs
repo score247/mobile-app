de8937ce95d72937336609cb5c949fd5178d865e
Harrison Nguyen <harrison.nguyen@starixsoft.com>
2019-11-07 10:39:01 +0700

    Clean code

--- src/Shared/Sports/Soccer/ViewModels/MatchDetails/TrackerCommentary/TrackerCommentaryViewModel.cs
+++ src/Shared/Sports/Soccer/ViewModels/MatchDetails/TrackerCommentary/TrackerCommentaryViewModel.cs
@@ -162,10 +162,10 @@ namespace LiveScore.Soccer.ViewModels.MatchDetails.TrackerCommentary
             {
                 var matchCommentaries = (await soccerMatchService
                         .GetMatchCommentariesAsync(matchCoverage.MatchId, CurrentLanguage))
-                    .Where(c => c.Commentaries?.Any() == true || c.TimelineType.IsHighlightEvent())
-                    .OrderByDescending(t => t.MatchTime)
-                    .ThenByDescending(t => t.Time)
-                    .ToList();
+                        .Where(c => c.Commentaries?.Any() == true || c.TimelineType.IsHighlightEvent())
+                        .OrderByDescending(t => t.MatchTime)
+                        .ThenByDescending(t => t.Time)
+                        .ToList();
 
                 if (matchCommentaries.Count > 0)
                 {
