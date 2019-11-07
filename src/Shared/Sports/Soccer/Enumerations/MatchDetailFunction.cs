38e0d7296055b8111cb6b0452accfe86eac2ce04
Vivian <vivian.nguyen@starixsoft.com>
2019-11-07 17:11:49 +0700

    000 - update tab index

--- src/Shared/Sports/Soccer/Enumerations/MatchDetailFunction.cs
+++ src/Shared/Sports/Soccer/Enumerations/MatchDetailFunction.cs
@@ -4,15 +4,15 @@ namespace LiveScore.Soccer.Enumerations
 {
     public class MatchDetailFunction : Enumeration
     {
-        public static readonly MatchDetailFunction Odds = new MatchDetailFunction(0, "Odds");
-        public static readonly MatchDetailFunction Info = new MatchDetailFunction(1, "SoccerMatch Info");
-        public static readonly MatchDetailFunction Tracker = new MatchDetailFunction(2, "Tracker");
-        public static readonly MatchDetailFunction Stats = new MatchDetailFunction(3, "Statistics");
-        public static readonly MatchDetailFunction Lineups = new MatchDetailFunction(4, "Line - ups");
-        public static readonly MatchDetailFunction H2H = new MatchDetailFunction(5, "Head to Head");
-        public static readonly MatchDetailFunction Table = new MatchDetailFunction(6, "Table");
-        public static readonly MatchDetailFunction Social = new MatchDetailFunction(7, "Social");
-        public static readonly MatchDetailFunction TV = new MatchDetailFunction(8, "TV Schedule");
+        //public static readonly MatchDetailFunction Odds = new MatchDetailFunction(0, "Odds");
+        public static readonly MatchDetailFunction Info = new MatchDetailFunction(0, "SoccerMatch Info");
+        public static readonly MatchDetailFunction Tracker = new MatchDetailFunction(1, "Tracker");
+        public static readonly MatchDetailFunction Stats = new MatchDetailFunction(2, "Statistics");
+        public static readonly MatchDetailFunction Lineups = new MatchDetailFunction(3, "Line - ups");
+        public static readonly MatchDetailFunction H2H = new MatchDetailFunction(4, "Head to Head");
+        public static readonly MatchDetailFunction Table = new MatchDetailFunction(5, "Table");
+        public static readonly MatchDetailFunction Social = new MatchDetailFunction(6, "Social");
+        public static readonly MatchDetailFunction TV = new MatchDetailFunction(7, "TV Schedule");
 
         public MatchDetailFunction()
         {
