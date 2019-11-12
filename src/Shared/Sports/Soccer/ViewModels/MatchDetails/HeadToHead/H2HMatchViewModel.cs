79cf89aca6cf187e226635c5c1befbd655d75862
Vivian <vivian.nguyen@starixsoft.com>
2019-11-12 13:32:34 +0700

    123431 - bold for selected team

--- src/Shared/Sports/Soccer/ViewModels/MatchDetails/HeadToHead/H2HMatchViewModel.cs
+++ src/Shared/Sports/Soccer/ViewModels/MatchDetails/HeadToHead/H2HMatchViewModel.cs
@@ -35,7 +35,11 @@ namespace LiveScore.Soccer.ViewModels.MatchDetails.HeadToHead
 
         public bool IsH2H { get; private set; }
 
-        public string Result { get; private set; }
+        public string Result { get; private set; } = string.Empty;
+
+        public bool IsHomeSelected { get; private set; }
+
+        public bool IsAwaySelected { get; private set; }
 
         public void BuildMatch(string teamId, IMatch match)
         {
@@ -45,13 +49,18 @@ namespace LiveScore.Soccer.ViewModels.MatchDetails.HeadToHead
             }
 
             Match = match;
-            DisplayEventDate = BuildDisplayEventDate(match);
+            DisplayEventDate = GetDisplayEventDate(match);
             DisplayMatchStatus = matchDisplayStatusBuilder.BuildDisplayStatus(Match);
 
-            Result = IsH2H ? string.Empty : BuildTeamResult(teamId, match);
+            if (!IsH2H)            
+            {
+                Result = GetTeamResult(teamId, match);
+                IsHomeSelected = teamId == match.HomeTeamId;
+                IsAwaySelected = !IsHomeSelected;
+            }
         }
 
-        private static string BuildDisplayEventDate(IMatch match)
+        private static string GetDisplayEventDate(IMatch match)
         {
             var currentYear = DateTimeOffset.Now.Year;
 
@@ -60,7 +69,7 @@ namespace LiveScore.Soccer.ViewModels.MatchDetails.HeadToHead
                 : match.EventDate.ToLocalYear();
         }
 
-        private static string BuildTeamResult(string teamId, IMatch match)
+        private static string GetTeamResult(string teamId, IMatch match)
         {
             if (string.IsNullOrWhiteSpace(match.WinnerId))
             {
