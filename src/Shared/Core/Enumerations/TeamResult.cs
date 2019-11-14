1bc88e0acf3f39944a3d24cfe3e5062c56f98a5e
Vivian <vivian.nguyen@starixsoft.com>
2019-11-14 10:24:51 +0700

    123431 - remove h2h converters and using data triggers

--- src/Shared/Core/Enumerations/TeamResult.cs
+++ src/Shared/Core/Enumerations/TeamResult.cs
@@ -2,9 +2,9 @@
 {
     public class TeamResult : Enumeration
     {
-        public static readonly TeamResult Win = new TeamResult(1, "win");
-        public static readonly TeamResult Draw = new TeamResult(2, "draw");
-        public static readonly TeamResult Lose = new TeamResult(3, "lose");
+        public static readonly TeamResult Win = new TeamResult(1, "W");
+        public static readonly TeamResult Draw = new TeamResult(2, "D");
+        public static readonly TeamResult Lose = new TeamResult(3, "L");
 
         public TeamResult()
         {
