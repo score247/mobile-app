a2cd2df6f3d48b7307c7b4099a1a79e048b80dd7
alice.le <alice.le@nexcel.vn>
2019-11-21 13:58:59 +0700

    124076 - Implement league detail page

---
+++ src/Shared/Sports/Soccer/ViewResources/LeagueDetailViewResource.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace LiveScore.Soccer.ViewResources
+{
+    using Xamarin.Forms;
+    using Xamarin.Forms.Xaml;
+
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LeagueDetailViewResource : ResourceDictionary
+    {
+        public LeagueDetailViewResource()
+        {
+            InitializeComponent();
+        }
+    }
+}
