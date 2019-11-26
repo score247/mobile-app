48d115e0fea61606353dedfd15b3337800a32ee0
Harrison Nguyen <harrison.nguyen@starixsoft.com>
2019-11-26 16:28:27 +0700

    124333 - Bring ListView back to Match Detail

---
+++ src/Platforms/LiveScore.iOS/Renderers/TabItemListViewRenderer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using LiveScore.Core.Controls.TabStrip;
+using LiveScore.iOS.Renderers;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(TabItemListView), typeof(TabItemListViewRenderer))]
+
+namespace LiveScore.iOS.Renderers
+{
+    public class TabItemListViewRenderer : ListViewRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<ListView> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control != null)
+            {
+                var tvDelegate = new TableViewDelegate();
+
+                Control.Delegate = tvDelegate;
+                tvDelegate.OnScrolled += TvDelegate_OnScrolled;
+            }
+        }
+
+        protected internal TabItemListView Source => Element as TabItemListView;
+
+        private void TvDelegate_OnScrolled(object sender, EventArgs e)
+        {
+            var scrollY = Control.ContentOffset.Y;
+            TabItemListView.OnScrolling(scrollY > 0 ? scrollY : 0);
+        }
+    }
+
+    public class TableViewDelegate : UITableViewDelegate
+    {
+        public event EventHandler OnScrolled;
+
+        public override void Scrolled(UIScrollView scrollView)
+        {
+            OnScrolled?.Invoke(scrollView, EventArgs.Empty);
+        }
+    }
+}
---
+++ src/Shared/Core/Controls/TabStrip/TabItemListView.cs
@@ -0,0 +1,22 @@
+﻿using LiveScore.Core.Controls.TabStrip.EventArgs;
+
+namespace LiveScore.Core.Controls.TabStrip
+{
+    using Xamarin.Forms;
+
+    public class TabItemListView : ListView
+    {
+        public TabItemListView() : base(ListViewCachingStrategy.RecycleElement)
+        {
+            RefreshControlColor = Color.White;
+        }
+
+        public static void OnScrolling(double offset)
+        {
+            MessagingCenter.Send(
+                nameof(TabStrip),
+                TabItemScrollingEventArgs.EventName,
+                new TabItemScrollingEventArgs(offset));
+        }
+    }
+}
