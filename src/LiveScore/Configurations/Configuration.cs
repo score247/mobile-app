4816aecf817f9a8024404e817b12a3af5aae2bd2
Vivian <vivian.nguyen@starixsoft.com>
2019-12-07 16:15:34 +0700

    000 - update configurations

--- src/LiveScore/Configurations/Configuration.cs
+++ src/LiveScore/Configurations/Configuration.cs
@@ -10,7 +10,7 @@ namespace LiveScore.Configurations
         public string AssetsEndPoint => "https://assets-dev.nexdev.net/test/";
         public string AppCenterSecret => "ios=b08e2753-b596-44c9-bed9-701d8dd8be8c;";
         public string SentryDsn => "https://a75e3e7b51ea4de8baa2c27b67bbede3@sentry.nexdev.net/34";
-        public string Enviroment => "Local";
+        public string Environment => "Local";
         public bool Debug => true;
         public string EncryptKey => "530c4b6a5f7451912b270aa140712e52";
 
@@ -21,7 +21,7 @@ namespace LiveScore.Configurations
         public string AssetsEndPoint => "https://assets-dev.nexdev.net/test/";
         public string AppCenterSecret => "ios=b08e2753-b596-44c9-bed9-701d8dd8be8c;";
         public string SentryDsn => "https://a75e3e7b51ea4de8baa2c27b67bbede3@sentry.nexdev.net/34";
-        public string Enviroment => "Local";
+        public string Environment => "Local";
         public bool Debug => true;
         public string EncryptKey => "e6e42fa2cd8db55e10c2b7b4833e0f8e";
 
@@ -32,7 +32,7 @@ namespace LiveScore.Configurations
         public string AssetsEndPoint => "https://assets-dev.nexdev.net/test/";
         public string AppCenterSecret => "ios=b08e2753-b596-44c9-bed9-701d8dd8be8c;";
         public string SentryDsn => "https://a75e3e7b51ea4de8baa2c27b67bbede3@sentry.nexdev.net/34";
-        public string Enviroment => "Local";
+        public string Environment => "Local";
         public bool Debug => true;
         public string EncryptKey => "350cd642f6764a154a9e4f03eb3121b6";
 
@@ -43,7 +43,7 @@ namespace LiveScore.Configurations
         public string AssetsEndPoint => "https://assets-dev.nexdev.net/main/";
         public string AppCenterSecret => "ios=34adf4e9-18dd-4ef0-817f-48bce4ff7159;";
         public string SentryDsn => "https://a75e3e7b51ea4de8baa2c27b67bbede3@sentry.nexdev.net/34";
-        public string Enviroment => "Testflight";
+        public string Environment => "Testflight";
         public bool Debug => false;
         public string EncryptKey => "350cd642f6764a154a9e4f03eb3121b6";
 
