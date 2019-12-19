d1fe1a0940cbd7c3223ba861b2df74834e528880
alice.le <alice.le@nexcel.vn>
2019-12-19 15:46:39 +0700

    125716-Edit UI for League page

---
+++ src/LiveScore.Features/League/ViewResources/LeagueViewResource.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace LiveScore.Features.League.ViewResources
+{
+    using Xamarin.Forms;
+    using Xamarin.Forms.Xaml;
+
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LeagueViewResource : ResourceDictionary
+    {
+        public LeagueViewResource()
+        {
+            InitializeComponent();
+        }
+    }
+}
