2288447ded3d17efad2544954870c702f77735aa
Vivian <vivian.nguyen@starixsoft.com>
2019-12-20 17:00:11 +0700

    000 - ignore aggregate exception

--- src/Shared/Common/Extensions/ExceptionExtension.cs
+++ src/Shared/Common/Extensions/ExceptionExtension.cs
@@ -7,6 +7,7 @@ namespace LiveScore.Common.Extensions
     {
         public static bool IsIgnore(this Exception ex)
             => ex.GetType() == typeof(OperationCanceledException) ||
-                ex.GetType() == typeof(TaskCanceledException);
+                ex.GetType() == typeof(TaskCanceledException) ||
+                ex.GetType() == typeof(AggregateException);
     }
 }
