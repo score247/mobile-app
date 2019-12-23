fdd1a97abf24899976ff1892133ea2f2c6aa8a0c
Harrison Nguyen <harrison.nguyen@starixsoft.com>
2019-12-23 17:47:13 +0700

    125849 - Remove year in league season dates

--- src/Shared/Core/Models/Leagues/LeagueSeasonDates.cs
+++ src/Shared/Core/Models/Leagues/LeagueSeasonDates.cs
@@ -6,17 +6,14 @@ namespace LiveScore.Core.Models.Leagues
     [MessagePackObject(keyAsPropertyName: true)]
     public class LeagueSeasonDates
     {
-        public LeagueSeasonDates(DateTimeOffset startDate, DateTimeOffset endDate, string year)
+        public LeagueSeasonDates(DateTimeOffset startDate, DateTimeOffset endDate)
         {
             StartDate = startDate;
             EndDate = endDate;
-            Year = year;
         }
 
         public DateTimeOffset StartDate { get; private set; }
 
         public DateTimeOffset EndDate { get; private set; }
-
-        public string Year { get; private set; }
     }
 }
