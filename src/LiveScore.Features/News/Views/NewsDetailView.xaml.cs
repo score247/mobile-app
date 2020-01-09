4b7082f277df2c784d0629472c63fd67cc2bb1e3
alice.le <alice.le@nexcel.vn>
2020-01-09 14:17:19 +0700

    126875-Implement UI for News List and News Detail pages

---
+++ src/LiveScore.Features/News/Views/NewsDetailView.xaml.cs
@@ -0,0 +1,33 @@
+﻿using LiveScore.Features.News.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LiveScore.Features.News.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NewsDetailView : ContentPage
+    {
+        public NewsDetailView()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            if (BindingContext is NewsViewModel viewModel)
+            {
+                viewModel.IsActive = true;
+                viewModel.OnAppearing();
+            }
+        }
+
+        protected override void OnDisappearing()
+        {
+            if (BindingContext is NewsViewModel viewModel)
+            {
+                viewModel.IsActive = false;
+                viewModel.OnDisappearing();
+            }
+        }
+    }
+}
