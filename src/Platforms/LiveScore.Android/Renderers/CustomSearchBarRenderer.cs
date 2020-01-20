1a9e0892cf9886bb32ea977a74bcff5c8eb7e240
Egan Le <egan.le@nexcel.vn>
2020-01-20 16:03:06 +0700

    000 - Add custom renderer search bar android

---
+++ src/Platforms/LiveScore.Android/Renderers/CustomSearchBarRenderer.cs
@@ -0,0 +1,41 @@
+﻿using Android.Content;
+using Android.Widget;
+using G = Android.Graphics;
+
+using LiveScore.Droid.Renderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(SearchBar), typeof(CustomSearchBarRenderer))]
+
+namespace LiveScore.Droid.Renderers
+{
+    public class CustomSearchBarRenderer : SearchBarRenderer
+    {
+        public CustomSearchBarRenderer(Context context) : base(context)
+        {
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<SearchBar> e)
+        {
+            base.OnElementChanged(e);
+            if (Control != null)
+            {
+                Control.Background = Context.GetDrawable(Resource.Drawable.custom_search_view);
+            }
+
+            var backGroundColor = ((Color)App.Current.Resources["SearchBarTextBoxBackgroundColor"]).ToAndroid();
+            var textColor = ((Color)App.Current.Resources["PrimaryTextColor"]).ToAndroid();
+
+            SearchView searchView = base.Control;
+            var textViewId = searchView.Context.Resources.GetIdentifier("android:id/search_src_text", null, null);
+            EditText textView = (searchView.FindViewById(textViewId) as EditText);
+            textView.SetBackgroundColor(backGroundColor);
+            textView.SetTextColor(textColor);
+
+            var searchIconId = searchView.Resources.GetIdentifier("android:id/search_mag_icon", null, null);
+            var searchPlateIcon = searchView.FindViewById(searchIconId);
+            (searchPlateIcon as ImageView).SetColorFilter(textColor, G.PorterDuff.Mode.SrcIn);
+        }
+    }
+}
