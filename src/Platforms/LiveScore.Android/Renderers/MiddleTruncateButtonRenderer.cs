c54e750c93b58767685b9f36f69f514c503e7048
Egan Le <egan.le@nexcel.vn>
2020-01-23 10:11:13 +0700

    000 - Add middle truncate for button in android renderer

---
+++ src/Platforms/LiveScore.Android/Renderers/MiddleTruncateButtonRenderer.cs
@@ -0,0 +1,24 @@
+﻿using Android.Content;
+using LiveScore.Droid.Renderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using static Android.Text.TextUtils;
+
+[assembly: ExportRenderer(typeof(Button), typeof(MiddleTruncateButtonRenderer))]
+
+namespace LiveScore.Droid.Renderers
+{
+    public class MiddleTruncateButtonRenderer : ButtonRenderer
+    {
+        public MiddleTruncateButtonRenderer(Context context) : base(context)
+        {
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Button> e)
+        {
+            base.OnElementChanged(e);
+            Control.SetMaxLines(1);
+            Control.Ellipsize = TruncateAt.Middle;
+        }
+    }
+}
