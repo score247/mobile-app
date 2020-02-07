c3c36d42ca4705a1cffd8bf849b30ad67d890cc6
Egan Le <egan.le@nexcel.vn>
2020-02-07 18:12:21 +0700

    000 - Add box shadow in android

--- src/Platforms/LiveScore.Android/Renderers/MenuTabbedViewRenderer.cs
+++ src/Platforms/LiveScore.Android/Renderers/MenuTabbedViewRenderer.cs
@@ -4,12 +4,14 @@ using System.Linq;
 using Android.Content;
 using Android.Support.Design.Widget;
 using Android.Views;
+using Android.Widget;
 using LiveScore.Droid.Renderers;
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.Android;
 using Xamarin.Forms.Platform.Android.AppCompat;
 
 [assembly: ExportRenderer(typeof(TabbedPage), typeof(MenuTabbedViewRenderer))]
+
 namespace LiveScore.Droid.Renderers
 {
     public class MenuTabbedViewRenderer : TabbedPageRenderer
@@ -19,7 +21,6 @@ namespace LiveScore.Droid.Renderers
         public MenuTabbedViewRenderer(Context context)
             : base(context)
         {
-
         }
 
         protected override void OnLayout(bool changed, int l, int t, int r, int b)
@@ -44,6 +45,29 @@ namespace LiveScore.Droid.Renderers
             }
         }
 
+        protected override void OnElementChanged(ElementChangedEventArgs<TabbedPage> e)
+        {
+            base.OnElementChanged(e);
+
+            if (!(GetChildAt(0) is ViewGroup layout))
+            {
+                return;
+            }
+
+            if (!(layout.GetChildAt(1) is BottomNavigationView bottomNavigationView))
+            {
+                return;
+            }
+
+            var topShadow = LayoutInflater.From(Context).Inflate(Resource.Drawable.view_shadow, null);
+
+            var layoutParams =
+                new Android.Widget.RelativeLayout.LayoutParams(LayoutParams.MatchParent, 15);
+            layoutParams.AddRule(LayoutRules.Above, bottomNavigationView.Id);
+
+            layout.AddView(topShadow, 2, layoutParams);
+        }
+
         private List<Android.Views.View> GetAllChildViews(Android.Views.View view)
         {
             if (!(view is ViewGroup group))
@@ -62,9 +86,7 @@ namespace LiveScore.Droid.Renderers
                 result.AddRange(childList);
             }
 
-            var color = App.Current.Resources["FunctionBarActiveColor"];
             return result.Distinct().ToList();
         }
-
     }
 }
