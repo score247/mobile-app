0ac9d6e79b092865a1b6153f2cafaae8c3678d1d
alice.le <alice.le@nexcel.vn>
2020-02-14 13:03:46 +0700

    127998-Create AboutScore247View page

---
+++ src/LiveScore.Features/Menu/ViewResources/AboutScore247ViewResource.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace LiveScore.Features.Menu.ViewResources
+{
+    using Xamarin.Forms;
+    using Xamarin.Forms.Xaml;
+
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AboutScore247ViewResource : ResourceDictionary
+    {
+        public AboutScore247ViewResource()
+        {
+            InitializeComponent();
+        }
+    }
+}
