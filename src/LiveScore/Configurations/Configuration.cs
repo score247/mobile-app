0fc1b85641029ae188a6b8d8c03eb5a597990112
harrison.nguyen@starixsoft.com <Harrison Nguyen>
2020-02-25 11:00:18 +0700

    000 - Update test  config

--- src/LiveScore/Configurations/Configuration.cs
+++ src/LiveScore/Configurations/Configuration.cs
@@ -19,7 +19,7 @@ namespace LiveScore.Configurations
         public string ApiEndPoint => "https://score247-api1.nexdev.net/test/api";
         public string ImageEndPoint => "https://score247-api1.nexdev.net/test/news/images";
         public string SignalRHubEndPoint => "https://score247-api2.nexdev.net/test/hubs";
-        public string AssetsEndPoint => "https://assets-dev.nexdev.net/test/";
+        public string AssetsEndPoint => "https://assets-dev.nexdev.net/";
         public string AppCenterSecret => "ios=b08e2753-b596-44c9-bed9-701d8dd8be8c;";
         public string SentryDsn => "https://4cf636af675645359c573f1b923693f8@sentry.nexdev.net/88";
         public string Environment => "Local";
